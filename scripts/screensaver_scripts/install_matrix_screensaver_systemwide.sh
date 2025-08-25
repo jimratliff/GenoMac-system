e5254be9cb82ab3d7851a63269085cc803991758
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-08-24 21:45:15 -0700

    Update install_matrix_screensaver_systemwide.sh

--- scripts/screensaver_scripts/install_matrix_screensaver_systemwide.sh
+++ scripts/screensaver_scripts/install_matrix_screensaver_systemwide.sh
@@ -47,7 +47,7 @@ function install_matrix_screensaver_systemwide() {
   local _raw_version
   set +e # Turning on for diagnostic purposes
   report "Visiting PlistBuddy: Wish me luck!"
-  _raw_version="$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$plist_path/Contents/Info.plist" 2>/dev/null)"
+  _raw_version="$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$plist_path" 2>/dev/null)"
   local plist_status=$?
   set -e # Turning off special diagnostic stuff
   success_or_not "$plist_status"
