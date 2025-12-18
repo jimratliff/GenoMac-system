fecb180d2c6a40aaf6957d2cd096be4ea788476d
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2025-12-17 20:12:18 -0800

    Revise install_uh_oh_sound_systemwide() to use helper
    Uses helper function: copy_resource_between_local_directories()

--- scripts/resource_installation/install_uh_oh_sound_systemwide.sh
+++ scripts/resource_installation/install_uh_oh_sound_systemwide.sh
@@ -13,6 +13,30 @@ source "${GENOMAC_HELPER_DIR}/helpers.sh"
 
 function install_uh_oh_sound_systemwide() {
   # Installs the Uh_oh.aiff alert sound systemwide (all users)
+  
+  report_start_phase_standard
+  
+  local sound_name="Uh_oh.aiff"
+  local system_alerts_dir="/Library/Audio/Sounds/Alerts"
+  local destination_path="${system_alerts_dir}/${sound_name}"
+  
+  # Construct full source path for this specific repo
+  local source_path="${GENOMAC_SYSTEM_LOCAL_DIRECTORY}/resources/sounds/alerts/${sound_name}"
+  
+  # Use the helper function to copy the resource
+  copy_resource_between_local_directories \
+    "$source_path" \
+    "$destination_path" \
+    --systemwide
+  
+  local result=$?
+  
+  report_end_phase_standard
+  return $result
+}
+
+function install_uh_oh_sound_systemwide_deprecated() {
+  # Installs the Uh_oh.aiff alert sound systemwide (all users)
 
   report_start_phase_standard
 
