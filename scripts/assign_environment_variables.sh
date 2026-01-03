2f7caca910bfa3ffe5c566626d8c51bd5cc76975
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-02 16:01:15 -0800

    Update assign_environment_variables.sh

--- scripts/assign_environment_variables.sh
+++ scripts/assign_environment_variables.sh
@@ -12,8 +12,6 @@
 
 set -euo pipefail
 
-
-
 # Resolve directory of the current script
 this_script_path="${0:A}"
 this_script_dir="${this_script_path:h}"
@@ -35,30 +33,12 @@ source "${GENOMAC_HELPER_DIR}/helpers.sh"
 # accessed during user-account creation.
 GENOMAC_USER_LOGIN_PICTURES_DIRECTORY="$HOME/.genomac-user-login-pictures"
 
-############### CONJECTURE: The following is used only by GenoMac-system
-
-# Specify URL for cloning the public GenoMac-system repository using HTTPS
-GENOMAC_SYSTEM_REPO_URL="https://github.com/jimratliff/GenoMac-system.git"
-
 # Specify local directory into which the GenoMac-system repository will be 
 # cloned
 # Note: This repo is cloned only by USER_CONFIGURER.
 GENOMAC_SYSTEM_LOCAL_DIRECTORY="$HOME/.genomac-system"
 
-############### CONJECTURE: The following is used only by GenoMac-user
 
-# Specify the location of the user’s `Dropbox` directory
-GENOMAC_USER_DROPBOX_DIRECTORY="$HOME/Library/CloudStorage/Dropbox"
-
-# Specify the local directory in which preferences and other files shared across users are stored
-# These may contain secrets, so this directory is NOT within a repo
-# E.g., this would be within each user’s Dropbox directory.
-GENOMAC_USER_SHARED_PREFERENCES_DIRECTORY="${GENOMAC_USER_DROPBOX_DIRECTORY}/Preferences_common"
-
-# Specify the file name of the BetterTouchTool (BTT) preset to be auto-loaded at BTT startup
-GENOMAC_USER_BTT_AUTOLOAD_PRESET_FILENAME="Default_preset.json"
-GENOMAC_USER_BTT_AUTOLOAD_PRESET_DIRECTORY="$HOME/.config/BetterTouchTool"
-GENOMAC_USER_BTT_AUTOLOAD_PRESET_PATH="${GENOMAC_USER_BTT_AUTOLOAD_PRESET_DIRECTORY}/${GENOMAC_USER_BTT_AUTOLOAD_PRESET_FILENAME}"
 
 # Export environment variables to be available in all subsequent shells
 report_action_taken "Exporting environment variables to be consistently available."
@@ -69,18 +49,6 @@ function export_and_report() {
   export "$var_name"
 }
 
-
-export_and_report GENOMAC_ALERT_LOG
 export_and_report GENOMAC_HELPER_DIR
-export_and_report GENOMAC_NAMESPACE
-export_and_report GENOMAC_STATE_FILE_EXTENSION
 export_and_report GENOMAC_SYSTEM_LOCAL_DIRECTORY
-export_and_report GENOMAC_SYSTEM_REPO_URL
-export_and_report GENOMAC_USER_DROPBOX_DIRECTORY
-export_and_report GENOMAC_USER_LOCAL_DIRECTORY
-export_and_report GENOMAC_USER_LOCAL_RESOURCE_DIRECTORY
-export_and_report GENOMAC_USER_LOCAL_STATE_DIRECTORY
 export_and_report GENOMAC_USER_LOGIN_PICTURES_DIRECTORY
-export_and_report GENOMAC_USER_REPO_URL
-export_and_report GENOMAC_USER_SHARED_PREFERENCES_DIRECTORY
-
