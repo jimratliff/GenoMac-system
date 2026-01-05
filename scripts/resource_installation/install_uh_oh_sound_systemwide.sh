9fa8f18946f1e349e07d636f240d494a50c88e74
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-04 16:55:14 -0800

    Update install_uh_oh_sound_systemwide.sh

--- scripts/resource_installation/install_uh_oh_sound_systemwide.sh
+++ scripts/resource_installation/install_uh_oh_sound_systemwide.sh
@@ -1,27 +1,18 @@
-# This file assumes GENOMAC_HELPER_DIR is already set in the current shell
-# to the absolute path of the directory containing helpers.sh.
-# That variable must be defined before this file is sourced.
-
-if [[ -z "${GENOMAC_HELPER_DIR:-}" ]]; then
-  echo "❌ GENOMAC_HELPER_DIR is not set. Please source install_resources.sh first."
-  return 1
-fi
-
-source "${GENOMAC_HELPER_DIR}/helpers.sh"
-
-############################## BEGIN SCRIPT PROPER ##############################
+#!/bin/zsh
 
 function install_uh_oh_sound_systemwide() {
   # Installs the Uh_oh.aiff alert sound systemwide (all users)
   
   report_start_phase_standard
   
-  local sound_name="Uh_oh.aiff"
-  local system_alerts_dir="/Library/Audio/Sounds/Alerts"
-  local destination_path="${system_alerts_dir}/${sound_name}"
-  
-  # Construct full source path for this specific repo
-  local source_path="${GENOMAC_SYSTEM_LOCAL_DIRECTORY}/resources/sounds/alerts/${sound_name}"
+  # Construct path to stored custom alert-sound file
+  # Hint: CUSTOM_ALERT_SOUND_FILENAME="Uh_oh.aiff"
+  # Hint: GENOMAC_SYSTEM_LOCAL_RESOURCE_DIRECTORY="${GENOMAC_SYSTEM_LOCAL_DIRECTORY}/resources"
+  local source_path="${GENOMAC_SYSTEM_LOCAL_RESOURCE_DIRECTORY}/sounds/alerts/${CUSTOM_ALERT_SOUND_FILENAME}"
+
+  # Destination directory is macOS-standard directory for storing available alert sounds
+  # Hint: SYSTEM_ALERT_SOUNDS_DIRECTORY="/Library/Audio/Sounds/Alerts"
+  local destination_path="${SYSTEM_ALERT_SOUNDS_DIRECTORY}/${sound_name}"
   
   # Use the helper function to copy the resource
   copy_resource_between_local_directories \
