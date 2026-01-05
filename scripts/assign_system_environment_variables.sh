fcfdbbeec4402d4eccf31867ae30a97e68d158eb
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-04 16:54:54 -0800

    Update assign_system_environment_variables.sh

--- scripts/assign_system_environment_variables.sh
+++ scripts/assign_system_environment_variables.sh
@@ -4,25 +4,9 @@
 
 set -euo pipefail
 
-# Resolve directory of the current script
-this_script_path="${0:A}"
-this_script_dir="${this_script_path:h}"
-
-# Specify the directory in which the `helpers.sh` file lives.
-# E.g., when `helpers.sh` lives at the same level as this script:
-# GENOMAC_HELPER_DIR="${this_script_dir}"
-GENOMAC_HELPER_DIR="${this_script_dir}"
-
-# Print assigned paths for diagnostic purposes
-printf "\n📂 Path diagnostics:\n"
-printf "this_script_dir:                  %s\n" "$this_script_dir"
-printf "GENOMAC_HELPER_DIR:               %s\n" "$GENOMAC_HELPER_DIR"
-
-# Source the helpers script
-source "${GENOMAC_HELPER_DIR}/helpers.sh"
-
 # Specify the local directory in which user login pictures are stored to be
 # accessed during user-account creation.
+# QUERY: IS THIS CORRECT? DO THESE RESIDE IN CONFIGGER’S HOME DIRECTORY?
 GENOMAC_USER_LOGIN_PICTURES_DIRECTORY="$HOME/.genomac-user-login-pictures"
 
 # Specify local directory into which the GenoMac-system repository will be 
@@ -36,12 +20,6 @@ GENOMAC_SYSTEM_LOCAL_RESOURCE_DIRECTORY="${GENOMAC_SYSTEM_LOCAL_DIRECTORY}/resou
 
 # Export environment variables to be available in all subsequent shells
 report_action_taken "Exporting environment variables to be consistently available."
-
-function export_and_report() {
-  local var_name="$1"
-  report "export $var_name: '${(P)var_name}'"
-  export "$var_name"
-}
 
 export_and_report GENOMAC_HELPER_DIR
 export_and_report GENOMAC_SYSTEM_LOCAL_DIRECTORY
