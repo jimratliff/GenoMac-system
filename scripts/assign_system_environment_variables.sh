d8a1b6c75564574b03f16268fde57ee802cb5a27
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-17 12:46:43 -0800

    Update assign_system_environment_variables.sh

--- scripts/assign_system_environment_variables.sh
+++ scripts/assign_system_environment_variables.sh
@@ -18,19 +18,8 @@ GENOMAC_SYSTEM_LOCAL_DIRECTORY="$HOME/.genomac-system"
 # operations by GenoMac-system
 GENOMAC_SYSTEM_LOCAL_RESOURCE_DIRECTORY="${GENOMAC_SYSTEM_LOCAL_DIRECTORY}/resources"
 
-# Export environment variables to be available in all subsequent shells
-# TODO: Revisit below commented-out line after refactoring (to use GenoMac-shared)
-# report_action_taken "Exporting environment variables to be consistently available."
-echo "Exporting environment variables to be consistently available."
+report_action_taken "Export environment variables to be available in all subsequent shells."
 
-# TODO: After refactoring is completed (viz., to reply on GenoMac-shared), this function definition can be deleted
-function export_and_report() {
-  local var_name="$1"
-  report "export $var_name: '${(P)var_name}'"
-  export "$var_name"
-}
-
-export_and_report GENOMAC_HELPER_DIR
 export_and_report GENOMAC_SYSTEM_LOCAL_DIRECTORY
 export_and_report GENOMAC_SYSTEM_LOCAL_RESOURCE_DIRECTORY
 export_and_report GENOMAC_USER_LOGIN_PICTURES_DIRECTORY
