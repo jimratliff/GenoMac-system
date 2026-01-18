dd8cb64f1e4dcf4e39150a099ca7466d894ca278
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-17 18:22:50 -0800

    Update implement_systemwide_prefs.sh

--- scripts/implement_systemwide_prefs.sh
+++ scripts/implement_systemwide_prefs.sh
@@ -5,40 +5,8 @@
 # Fail early on unset variables or command failure
 set -euo pipefail
 
-# Resolve this script's directory (even if sourced)
-this_script_path="${0:A}"
-this_script_dir="${this_script_path:h}"
+safe_source "${GMS_PREFS_SCRIPTS_DIR}/set_initial_systemwide_settings.sh"
 
-# Ensure Homebrew is in PATH for this script
-# The following shouldn't be necessary, so I'm commenting it out
-# eval "$(/opt/homebrew/bin/brew shellenv)"
-
-# Assign environment variables (including GENOMAC_HELPER_DIR).
-# Assumes that assign_environment_variables.sh is in same directory as the
-# current script.
-source "${this_script_dir}/assign_environment_variables.sh"
-
-# Source helpers
-source "${GENOMAC_HELPER_DIR}/helpers.sh"
-
-# Specify the directory in which the file(s) containing the preferences-related 
-# functions called by this script lives.
-# E.g., the function `get_loginwindow_message` is supplied by a file 
-# `get_loginwindow_message.sh`. Assuming `get_loginwindow_message.sh` 
-# resides at the same level as this script:
-# PREFS_FUNCTIONS_DIR="${this_script_dir}"
-PREFS_FUNCTIONS_DIR="${this_script_dir}/prefs_scripts"
-
-# Print assigned paths for diagnostic purposes
-printf "\n📂 Path diagnostics:\n"
-printf "this_script_dir:              %s\n" "$this_script_dir"
-printf "GENOMAC_HELPER_DIR: %s\n" "$GENOMAC_HELPER_DIR"
-printf "PREFS_FUNCTIONS_DIR:  %s\n\n" "$PREFS_FUNCTIONS_DIR"
-
-
-source "${PREFS_FUNCTIONS_DIR}/set_initial_systemwide_settings.sh"
-
-############################## BEGIN SCRIPT PROPER #############################
 report_start_phase 'Begin the systemwide-settings phase'
 
 # Set initial system-wide settings (requires sudo)
