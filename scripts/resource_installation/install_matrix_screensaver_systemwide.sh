a047ec0aa6ebca563561ea639ddb8330d17c2409
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-17 18:15:41 -0800

    Update install_matrix_screensaver_systemwide.sh

--- scripts/resource_installation/install_matrix_screensaver_systemwide.sh
+++ scripts/resource_installation/install_matrix_screensaver_systemwide.sh
@@ -1,15 +1,4 @@
-# This file assumes GENOMAC_HELPER_DIR is already set in the current shell
-# to the absolute path of the directory containing helpers.sh.
-# That variable must be defined before this file is sourced.
-
-if [[ -z "${GENOMAC_HELPER_DIR:-}" ]]; then
-  echo "❌ GENOMAC_HELPER_DIR is not set. Please source install_resources.sh first."
-  return 1
-fi
-
-source "${GENOMAC_HELPER_DIR}/helpers.sh"
-
-############################## BEGIN SCRIPT PROPER ##############################
+#!/usr/bin/env zs
 
 function install_matrix_screensaver_systemwide() {
   # Installs Monroe Williams’ “MatrixDownload” screensaver systemwide.
