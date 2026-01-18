b37fdbd56d53a2cee030d704453f44ea73fa926b
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-18 11:21:26 -0800

    Update set_initial_systemwide_settings.sh

--- scripts/prefs_scripts/set_initial_systemwide_settings.sh
+++ scripts/prefs_scripts/set_initial_systemwide_settings.sh
@@ -4,70 +4,70 @@ safe_source "${GMS_PREFS_SCRIPTS_DIR}/get_Mac_names.sh"
 safe_source "${GMS_PREFS_SCRIPTS_DIR}/get_loginwindow_message.sh"
 
 function set_initial_systemwide_settings() {
-# Makes system-wide settings, requiring sudo, to be run from USER_CONFIGURER.
-
-report_start_phase_standard
-
-report_action_taken "Begin commands that require 'sudo'"
-keep_sudo_alive
-
-# Get ComputerName and LocalHostName
-get_Mac_names
-
-# Get login-window message
-get_loginwindow_message
-
-# Disable auto-boot when opening the lid or connecting to power on Apple Silicon laptop
-# Howard Oakley, “How to change lid behaviour on MacBook Air and Pro,” Eclectic Light Company, February 3, 2025
-# https://eclecticlight.co/2025/02/03/how-to-change-lid-behaviour-on-macbook-air-and-pro/
-report_action_taken "Disable auto-boot when opening the lid or connecting to power on Apple Silicon laptop"
-sudo nvram BootPreference=%00 ; success_or_not
-
-# # Make Homebrew’s zsh an allowable shell
-# report_action_taken "Add Homebrew’s flavor of Zsh as allowable shell if possible/necessary"
-# HOMEBREW_PREFIX="$(/usr/bin/env brew --prefix)"   # e.g., /opt/homebrew
-# BREW_ZSH="$HOMEBREW_PREFIX/bin/zsh"
-# SHELLS_FILE="/etc/shells"
-# 
-# if [[ ! -x "$BREW_ZSH" ]]; then
-#   report_fail "$BREW_ZSH not found. Did 'brew bundle' install zsh yet?"
-#   exit 1
-# fi
-# 
-# if ! grep -qx "$BREW_ZSH" "$SHELLS_FILE" 2>/dev/null; then
-#   report_adjust_setting "Adding $BREW_ZSH to $SHELLS_FILE"
-#   echo "$BREW_ZSH" | sudo tee -a "$SHELLS_FILE" >/dev/null ; success_or_not
-# else
-#   report_success "$BREW_ZSH already present in $SHELLS_FILE"
-# fi
-
-# Configure application firewall
-report_action_taken "Configure application firewall"
-report_adjust_setting "1 of 2: Enable application firewall"
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on;success_or_not
-report_adjust_setting "2 of 2: Enable Stealth Mode"
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on;success_or_not
-
-# Configure system-wide settings controlling software-update behavior
-report_action_taken "Implement system-wide settings controlling how macOS and MAS-app software updates occur"
-
-report_adjust_setting "Automatically check for updates (both macOS and MAS apps)"
-sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true;success_or_not
-
-report_adjust_setting "Download updates when available"
-sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticDownload -bool true;success_or_not
-
-report_adjust_setting "Do NOT automatically update macOS"
-sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticallyInstallMacOSUpdates -bool false;success_or_not
-
-report_adjust_setting "Automatically update applications from Mac App Store"
-sudo defaults write /Library/Preferences/com.apple.commerce AutoUpdate -bool true;success_or_not
-
-# Display additional information on login window
-report_adjust_setting "Display additional info (IP address, hostname, OS version) when clicking on the clock digits of the login window"
-# Requires restart.
-sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName;success_or_not
-
-report_end_phase_standard
+  # Makes system-wide settings, requiring sudo, to be run from USER_CONFIGURER.
+  
+  report_start_phase_standard
+  
+  report_action_taken "Begin commands that require 'sudo'"
+  keep_sudo_alive
+  
+  # Get ComputerName and LocalHostName
+  get_Mac_names
+  
+  # Get login-window message
+  get_loginwindow_message
+  
+  # Disable auto-boot when opening the lid or connecting to power on Apple Silicon laptop
+  # Howard Oakley, “How to change lid behaviour on MacBook Air and Pro,” Eclectic Light Company, February 3, 2025
+  # https://eclecticlight.co/2025/02/03/how-to-change-lid-behaviour-on-macbook-air-and-pro/
+  report_action_taken "Disable auto-boot when opening the lid or connecting to power on Apple Silicon laptop"
+  sudo nvram BootPreference=%00 ; success_or_not
+  
+  # # Make Homebrew’s zsh an allowable shell
+  # report_action_taken "Add Homebrew’s flavor of Zsh as allowable shell if possible/necessary"
+  # HOMEBREW_PREFIX="$(/usr/bin/env brew --prefix)"   # e.g., /opt/homebrew
+  # BREW_ZSH="$HOMEBREW_PREFIX/bin/zsh"
+  # SHELLS_FILE="/etc/shells"
+  # 
+  # if [[ ! -x "$BREW_ZSH" ]]; then
+  #   report_fail "$BREW_ZSH not found. Did 'brew bundle' install zsh yet?"
+  #   exit 1
+  # fi
+  # 
+  # if ! grep -qx "$BREW_ZSH" "$SHELLS_FILE" 2>/dev/null; then
+  #   report_adjust_setting "Adding $BREW_ZSH to $SHELLS_FILE"
+  #   echo "$BREW_ZSH" | sudo tee -a "$SHELLS_FILE" >/dev/null ; success_or_not
+  # else
+  #   report_success "$BREW_ZSH already present in $SHELLS_FILE"
+  # fi
+  
+  # Configure application firewall
+  report_action_taken "Configure application firewall"
+  report_adjust_setting "1 of 2: Enable application firewall"
+  sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on;success_or_not
+  report_adjust_setting "2 of 2: Enable Stealth Mode"
+  sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on;success_or_not
+  
+  # Configure system-wide settings controlling software-update behavior
+  report_action_taken "Implement system-wide settings controlling how macOS and MAS-app software updates occur"
+  
+  report_adjust_setting "Automatically check for updates (both macOS and MAS apps)"
+  sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true;success_or_not
+  
+  report_adjust_setting "Download updates when available"
+  sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticDownload -bool true;success_or_not
+  
+  report_adjust_setting "Do NOT automatically update macOS"
+  sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticallyInstallMacOSUpdates -bool false;success_or_not
+  
+  report_adjust_setting "Automatically update applications from Mac App Store"
+  sudo defaults write /Library/Preferences/com.apple.commerce AutoUpdate -bool true;success_or_not
+  
+  # Display additional information on login window
+  report_adjust_setting "Display additional info (IP address, hostname, OS version) when clicking on the clock digits of the login window"
+  # Requires restart.
+  sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName;success_or_not
+  
+  report_end_phase_standard
 
 }
