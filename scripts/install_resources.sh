ef28576abc561082131882f20e94d7858e5781f2
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-17 18:15:46 -0800

    Update install_resources.sh

--- scripts/install_resources.sh
+++ scripts/install_resources.sh
@@ -1,43 +1,18 @@
-#!/bin/zsh
-
-# Installs fonts.
+#!/usr/bin/env zs
 
 # Fail early on unset variables or command failure
 set -euo pipefail
 
-# Resolve this script's directory (even if sourced)
-this_script_path="${0:A}"
-this_script_dir="${this_script_path:h}"
+# Template for entry-point scripts
 
-# Assign environment variables (including GENOMAC_HELPER_DIR).
-# Assumes that assign_environment_variables.sh is in same directory as this script.
-source "${this_script_dir}/assign_environment_variables.sh"
-
-# Source helpers
-source "${GENOMAC_HELPER_DIR}/helpers.sh"
-
-# Resource-related scripts should be placed in GenoMac-system/scripts/resource_installation
-
-# Specify the directory in which the file(s) containing the resource-related 
-# functions called by this script lives.
-# E.g., the function `install_resources_systemwide` is supplied by a file 
-# `install_resources_systemwide.sh`. Assuming `install_resources_systemwide.sh` 
-# resides at the same level as this script:
-# PREFS_RESOURCES_FUNCTIONS_DIR="${this_script_dir}"
-PREFS_RESOURCES_FUNCTIONS_DIR="${this_script_dir}/resource_installation"
-
-# Print assigned paths for diagnostic purposes
-printf "\n📂 Path diagnostics:\n"
-printf "this_script_dir:              %s\n" "$this_script_dir"
-printf "GENOMAC_HELPER_DIR: %s\n" "$GENOMAC_HELPER_DIR"
-printf "PREFS_RESOURCES_FUNCTIONS_DIR:  %s\n\n" "$PREFS_RESOURCES_FUNCTIONS_DIR"
+source "${HOME}/.genomac-system/scripts/0_initialize_me.sh"
 
 # Source function(s)
-source "${PREFS_RESOURCES_FUNCTIONS_DIR}/install_fira_code_nerd_font_systemwide.sh"
-source "${PREFS_RESOURCES_FUNCTIONS_DIR}/install_matrix_screensaver_systemwide.sh"
-source "${PREFS_RESOURCES_FUNCTIONS_DIR}/install_uh_oh_sound_systemwide.sh"
+# Resource-related scripts should be placed in GenoMac-system/scripts/resource_installation
+source "${GMS_RESOURCE_INSTALLATION_SCRIPTS_DIR}/install_fira_code_nerd_font_systemwide.sh"
+source "${GMS_RESOURCE_INSTALLATION_SCRIPTS_DIR}/install_matrix_screensaver_systemwide.sh"
+source "${GMS_RESOURCE_INSTALLATION_SCRIPTS_DIR}/install_uh_oh_sound_systemwide.sh"
 
-############################## BEGIN SCRIPT PROPER ##############################
 function install_resources_systemwide() {
   report_start_phase_standard
 
