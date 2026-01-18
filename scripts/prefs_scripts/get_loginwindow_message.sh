4ab15944f5b43800abae1709850f60dec9164615
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-18 11:19:52 -0800

    Update get_loginwindow_message.sh

--- scripts/prefs_scripts/get_loginwindow_message.sh
+++ scripts/prefs_scripts/get_loginwindow_message.sh
@@ -15,46 +15,34 @@ function get_loginwindow_message() {
   preexisting_text=$(defaults read /Library/Preferences/com.apple.loginwindow LoginwindowText 2>/dev/null || true)
   
   if [[ -n "$preexisting_text" ]]; then
-    echo "Preexisting login-window text: \"$preexisting_text\""
+    report "Preexisting login-window text: \"$preexisting_text\""
   else
-    echo "No existing login-window text."
+    report "No existing login-window text."
   fi
   
-  # Ask whether to set new login text
-  while true; do
-    echo -n "Would you like to change login-window text? (y/n): "
-    read choice
-    if [[ "$choice" =~ ^[YyNn]$ ]]; then
-      break
-    else
-      echo "Invalid response. Please enter “y” or “n”."
-    fi
-  done
-  
-  if [[ "$choice" =~ ^[Yy]$ ]]; then
+  # Ask whether to change login-window text
+  if get_yes_no_answer_to_question "Would you like to change login-window text?"; then
+    # Confirmation loop allowing blank input
     while true; do
-      echo -n "Enter desired login-window message (leave blank for none): "
-      read user_input
-  
-      echo "You entered: \"$user_input\""
-      echo -n "Is this correct? (y/n): "
-      read confirmation
-  
-      if [[ "$confirmation" =~ ^[Yy]$ ]]; then
+      ask_question "Enter desired login-window message (leave blank for none):"
+      read "user_input?→ "
+      
+      if get_yes_no_answer_to_question "You entered: \"$user_input\". Is this correct?"; then
         break
       fi
     done
   
-    echo "Final choice: \"$user_input\""
+    report "Final choice: \"$user_input\""
     if [[ -n "$user_input" ]]; then
+    	report_action_taken "New login-window message: ${user_input}"
       sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText -string "$user_input"; success_or_not
     else
+    	report_action_taken "Login-window message deleted"
       sudo defaults delete /Library/Preferences/com.apple.loginwindow LoginwindowText 2>/dev/null; success_or_not
     fi
   else
-    echo "No changes made to login-window text."
+    report "No changes made to login-window text."
   fi
   
   report_end_phase_standard
-
 }
