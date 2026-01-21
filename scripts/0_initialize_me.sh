cd556b89c38250a0280cc3be87b8bb7b3a2cca5f
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-20 21:30:41 -0800

    Update 0_initialize_me.sh

--- scripts/0_initialize_me.sh
+++ scripts/0_initialize_me.sh
@@ -61,7 +61,7 @@ function source_with_report() {
   # Ensures that an error is raised if a `source` of the file in the supplied argument fails.
   #
   # Defining this function here solves a chicken-or-egg problem: We’d like to use the helper 
-  # safe_source(), but it hasn’t been sourced yet. The current function is quite as full functional 
+  # safe_source(), but it hasn’t been sourced yet. The current function is not quite as full functional 
   # but will do for the initial sourcing of helpers.
   local file="$1"
   if source "$file"; then
