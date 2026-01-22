952058291daeeed380eec76ef83483daebb21e47
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-21 20:43:39 -0800

    Update run_hypervisor.sh

--- scripts/run_hypervisor.sh
+++ scripts/run_hypervisor.sh
@@ -12,7 +12,7 @@ safe_source "${GMS_PREFS_SCRIPTS}/implement_systemwide_settings.sh"
 safe_source "${GMS_PREFS_SCRIPTS}/install_via_homebrew.sh"
 safe_source "${GMS_PREFS_SCRIPTS}/interactive_get_Mac_names_and_login_window_message.sh"
 safe_source "${GMS_PREFS_SCRIPTS}/interactive_sign_into_MAS.sh"
-safe_source "${GMS_RESOURCE_INSTALLATION_SCRIPTS}/install_resources.sh"
+safe_source "${GMS_RESOURCE_INSTALL_SCRIPTS}/install_resources.sh"
 safe_source "${GMS_USER_SCOPE_SCRIPTS}/clone_genomac_user_repo.sh"
 
 ############### Context
