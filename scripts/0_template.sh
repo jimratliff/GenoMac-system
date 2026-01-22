b8367a2185ca0e407d0d14a57573556e64de18ae
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-22 10:32:17 -0800

    Update 0_template.sh

--- scripts/0_template.sh
+++ scripts/0_template.sh
@@ -10,7 +10,7 @@ source "${HOME}/.genomac-system/scripts/0_initialize_me.sh"
 ############################## BEGIN SCRIPT PROPER ##############################
 
 # Source required files
-# safe_source "${GMS_PREFS_SCRIPTS}/install_btt_license.sh"
+# safe_source "${GMS_SETTINGS_SCRIPTS}/install_btt_license.sh"
 
 function some_function() {
   report_start_phase_standard
