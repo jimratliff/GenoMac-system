94c9b576cff805abd2a0d8f3ec29750b773a8935
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-21 17:24:25 -0800

    Update install_default_browser_cli.sh

--- scripts/non-Homebrew_installation/install_default_browser_cli.sh
+++ scripts/non-Homebrew_installation/install_default_browser_cli.sh
@@ -1,28 +1,11 @@
 #!/bin/zsh
 
-# This file assumes GENOMAC_HELPER_DIR is already set in the current shell
-# to the absolute path of the directory containing helpers.sh.
-# That variable must be defined before this file is sourced.
-
-if [[ -z "${GENOMAC_HELPER_DIR:-}" ]]; then
-  echo "❌ GENOMAC_HELPER_DIR is not set. Please ensure helpers.sh has been loaded before sourcing install_tool_via_package_from_github.sh."
-  return 1
-fi
-
-source "${GENOMAC_HELPER_DIR}/helpers.sh"
-
-# Fail early on unset variables or command failure
-set -euo pipefail
-
-############################## SPECIFIC INSTALLERS ##############################
-
-# 1) default-browser (macadmins)
-#
-# NOTE: You need to set pkg_filename to match the *actual* asset name
-# from the v1.0.18 release page. The illustrative name below may need
-# tweaking once you eyeball the Releases tab.
-#
 function install_default_browser_cli() {
+  # Install default-browser from macadmins
+  #
+  # NOTE: You need to set pkg_filename to match the *actual* asset name
+  # from the v1.0.18 release page.
+  
   report_start_phase_standard
 
   local tool_name="default-browser"
