1fd41decaffbe62df0b81128df247b49028b8a46
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-21 17:58:28 -0800

    Update install_resources.sh

--- scripts/resource_installation/install_resources.sh
+++ scripts/resource_installation/install_resources.sh
@@ -21,14 +21,14 @@ function install_resources_systemwide() {
   report_start_phase_standard
 
   # Installs Fira Code Nerd Font
+  report_action_taken "Install Fira Code Nerd font"
   install_fira_code_nerd_font_systemwide
 
-  # Installs MatrixDownload screensaver
+  report_action_taken "Install MatrixDownload screensaver"
   install_matrix_screensaver_systemwide
 
-  # Installs Uh_oh alert sound
+  report_action_taken "Install ‘Uh oh!’ alert sound"
   install_uh_oh_sound_systemwide
 
   report_end_phase_standard
-
 }
