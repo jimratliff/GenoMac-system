75e976c01fa22c1e09c968e68c11b2a5303f81e0
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-22 12:34:33 -0800

    Update sentinel.sh

--- scripts/hypervisor/sentinel.sh
+++ scripts/hypervisor/sentinel.sh
@@ -2,12 +2,31 @@
 
 set -euo pipefail
 
-function update_repo() {
-  cd ~/.genomac-system
-  git pull --recurse-submodules origin main
-}
+
+
+
 
 function sentinel_of_the_hypervisor() {
   update_repo
 
+}
+
+function source_with_report() {
+  # Ensures that an error is raised if a `source` of the file in the supplied argument fails.
+  #
+  # Defining this function here solves a chicken-or-egg problem: We’d like to use the helper 
+  # safe_source(), but it hasn’t been sourced yet. The current function is not quite as full functional 
+  # but will do for the initial sourcing of helpers.
+  local file="$1"
+  if source "$file"; then
+    echo "Sourced: $file"
+  else
+    return "Failed to source: $file"
+    return 1
+  fi
+}
+
+function update_repo() {
+  cd ~/.genomac-system
+  git pull --recurse-submodules origin main
 }
