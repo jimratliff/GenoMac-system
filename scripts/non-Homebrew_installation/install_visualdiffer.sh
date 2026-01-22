34debceba5aec897e3b0e0bcbc3216aeee66f03c
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-21 17:25:00 -0800

    Update install_visualdiffer.sh

--- scripts/non-Homebrew_installation/install_visualdiffer.sh
+++ scripts/non-Homebrew_installation/install_visualdiffer.sh
@@ -1,20 +1,8 @@
-# This file assumes GENOMAC_HELPER_DIR is already set in the current shell
-# to the absolute path of the directory containing helpers.sh.
-# That variable must be defined before this file is sourced.
-
-if [[ -z "${GENOMAC_HELPER_DIR:-}" ]]; then
-  echo "❌ GENOMAC_HELPER_DIR is not set. Please ensure helpers.sh has been loaded before sourcing install_alan_app.sh."
-  return 1
-fi
-
-source "${GENOMAC_HELPER_DIR}/helpers.sh"
-
-############################## BEGIN SCRIPT PROPER ##############################
+#!/usr/bin/env zs
 
 function install_visualdiffer() {
   # Installs VisualDiffer macOS app, by Davide Ficano, et al., into /Applications.
   # Release page: https://github.com/Visualdiffer/visualdiffer/releases
-  # 🗑️ To uninstall: sudo rm -rf "/Applications/VisualDiffer.app"
 
   report_start_phase_standard
 
