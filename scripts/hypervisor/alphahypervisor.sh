8d1156d9a9a8932e53a80246c0f0e2912d2a242f
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-22 19:35:42 -0800

    Update alphahypervisor.sh

--- scripts/hypervisor/alphahypervisor.sh
+++ scripts/hypervisor/alphahypervisor.sh
@@ -10,29 +10,33 @@ function alphahypervisor() {
   #
   # It assumes that GenoMac-system has been cloned locally to GMS_LOCAL_DIRECTORY (~/.genomac-system).
   # It is *not* necessary to update the clone before running this function, because this function updates the clone.
-  #
-  # Side effects:
-  # - Exports
-  #   - GMS_LOCAL_DIRECTORY="$HOME/.genomac-system"
-  #   - GMS_SCRIPTS="${GMS_LOCAL_DIRECTORY}/scripts"
-  #   - GMS_HYPERVISOR_SCRIPTS="${GMS_SCRIPTS}/hypervisor"
+  
 
   echo "Inside alphahypervisor"
 
-  # Specify local directory into which the GenoMac-system repository will be cloned
-  GMS_LOCAL_DIRECTORY="$HOME/.genomac-system"
-  GMS_SCRIPTS="${GMS_LOCAL_DIRECTORY}/scripts"
-  GMS_HYPERVISOR_SCRIPTS="${GMS_SCRIPTS}/hypervisor"
-  
-  function export_and_report() {
-    local var_name="$1"
-    echo "export $var_name: '${(P)var_name}'"
-    export "$var_name"
-  }
-    
-  export_and_report "GMS_LOCAL_DIRECTORY"
-  export_and_report "GMS_SCRIPTS"
-  export_and_report "GMS_HYPERVISOR_SCRIPTS"
+  ############### Bootstrapping: Source initial initialization script
+  # This will make available:
+  # - Environment variables
+  #   - GMS_LOCAL_DIRECTORY      ~/.genomac-system
+  #   - GMS_SCRIPTS              ${GMS_LOCAL_DIRECTORY}/scripts
+  #   - GMS_HYPERVISOR_SCRIPTS   ${GMS_SCRIPTS}/hypervisor
+  # - Functions
+  #   - export_and_report
+  #   - source_with_report
+  #     - This will be disfavored relative to safe_source, once safe_source becomes available
+  #       after helper-misc.sh is sourced
+
+  # WARNING: This path is hard-wired and needs to be monitored for continued appropriateness
+  initial_initialization_script="$HOME/.genomac-system/scripts/0_initialize_me_first.sh"
+
+  if source "$initial_initialization_script" 2>/dev/null; then
+    echo "Sourced: $initial_initialization_script"
+  else
+    echo "Failed to source: $initial_initialization_script"
+    return 1
+  fi
+
+  ###
 
   # Updates the clone of GenoMac-system that is assumed to reside at GENOMAC_SYSTEM_LOCAL_DIRECTORY
   echo "Updating local clone of GenoMac-system"
@@ -41,20 +45,7 @@ function alphahypervisor() {
   # Now that GenoMac-system has been updated, spawn the hypervisor that manages the bootstrapping/maintenance
   # of the system-scoped configuration
 
-  function source_with_report() {
-    # Ensures that an error is raised if a `source` of the file in the supplied argument fails.
-    #
-    # Defining this function here solves a chicken-or-egg problem: We’d like to use the helper 
-    # safe_source(), but it hasn’t been sourced yet. The current function is not quite as full functional 
-    # but will do for the initial sourcing of helpers.
-    local file="$1"
-    if source "$file"; then
-      echo "Sourced: $file"
-    else
-      echo "Failed to source: $file"
-      return 1
-    fi
-  }
+
 
   initialization_script="$GMS_HYPERVISOR_SCRIPTS/0_initialize_me.sh"
   hypervisor_script="$GMS_HYPERVISOR_SCRIPTS/run_hypervisor.sh"
