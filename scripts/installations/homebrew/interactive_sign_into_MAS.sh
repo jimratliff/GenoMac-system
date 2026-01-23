4c176564a9e3a8839dfab242701ed8304b9ee8b2
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-22 20:20:21 -0800

    Update interactive_sign_into_MAS.sh

--- scripts/installations/homebrew/interactive_sign_into_MAS.sh
+++ scripts/installations/homebrew/interactive_sign_into_MAS.sh
@@ -15,7 +15,7 @@ function interactive_sign_into_MAS() {
   report_start_phase_standard
   
   launch_app_and_prompt_user_to_act \
-    --show-doc "${GENOMAC_SYSTEM_LOCAL_DOCS_TO_DISPLAY}/MAS_how_to_log_in.md" \
+    --show-doc "${GMS_DOCS_TO_DISPLAY}/MAS_how_to_log_in.md" \
     "$BUNDLE_ID_APP_STORE" \
     "Follow the instructions in the Quick Look window to log into the App Store"
     
