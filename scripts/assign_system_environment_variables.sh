45a588a3d80a1d0b7aaad3b062fa8651625f86bc
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-22 20:31:28 -0800

    Update assign_system_environment_variables.sh

--- scripts/assign_system_environment_variables.sh
+++ scripts/assign_system_environment_variables.sh
@@ -19,13 +19,13 @@ GMS_HYPERVISOR_SCRIPTS="${GMS_SCRIPTS}/hypervisor"
 
 # Specify the local directory that holds resources (files or folders) needed for particular
 # operations by GenoMac-system
-GENOMAC_SYSTEM_LOCAL_RESOURCE_DIRECTORY="${GENOMAC_SYSTEM_LOCAL_DIRECTORY}/resources"
+GMS_RESOURCES="${GMS_LOCAL_DIRECTORY}/resources"
 
 # Specify the local directory that holds documentation files to display to the executing user
-GMS_DOCS_TO_DISPLAY="${GENOMAC_USER_LOCAL_RESOURCE_DIRECTORY}/docs_to_display_to_user"
+GMS_DOCS_TO_DISPLAY="${GMS_RESOURCES}/docs_to_display_to_user"
 
 # Specify the local directory that holds declarative Homebrew files
-GENOMAC_SYSTEM_LOCAL_HOMEBREW_DIRECTORY="${GENOMAC_SYSTEM_LOCAL_DIRECTORY}/homebrew"
+GENOMAC_SYSTEM_LOCAL_HOMEBREW_DIRECTORY="${GMS_LOCAL_DIRECTORY}/homebrew"
 
 # Environment variables to support the Hypervisor
 GMS_HYPERVISOR_MAKE_COMMAND_STRING="make run-hypervisor"
@@ -34,9 +34,8 @@ GMS_HYPERVISOR_HOW_TO_RESTART_STRING="To get back into the groove at any time, j
 report_action_taken "Export environment variables to be available in all subsequent shells."
 
 export_and_report GMS_DOCS_TO_DISPLAY
-export_and_report GENOMAC_SYSTEM_LOCAL_DOCUMENTATION_DIRECTORY
 export_and_report GENOMAC_SYSTEM_LOCAL_HOMEBREW_DIRECTORY
-export_and_report GENOMAC_SYSTEM_LOCAL_RESOURCE_DIRECTORY
+export_and_report GMS_RESOURCES
 export_and_report GMS_HYPERVISOR_HOW_TO_RESTART_STRING
 export_and_report GMS_HYPERVISOR_MAKE_COMMAND_STRING
 
