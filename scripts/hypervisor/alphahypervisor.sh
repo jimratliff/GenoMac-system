dc642cf52ccf8670182b3229c13a003b35b36936
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-24 15:34:27 -0800

    Update alphahypervisor.sh

--- scripts/hypervisor/alphahypervisor.sh
+++ scripts/hypervisor/alphahypervisor.sh
@@ -8,7 +8,7 @@ function alphahypervisor() {
   #
   # This is the function called first, directly, and immediately by `make run-hypervisor`.
   #
-  # It assumes that GenoMac-system has been cloned locally to GMS_LOCAL_DIRECTORY (~/.genomac-system).
+  # It assumes that GenoMac-system has been cloned locally to GENOMAC_SYSTEM_LOCAL_DIRECTORY (~/.genomac-system).
   # It is *not* necessary to update the clone before running this function, because this function updates the clone.
   
 
@@ -17,9 +17,9 @@ function alphahypervisor() {
   ############### Bootstrapping: Source initial initialization script
   # This will make available:
   # - Environment variables
-  #   - GMS_LOCAL_DIRECTORY      ~/.genomac-system
-  #   - GMS_SCRIPTS              ${GMS_LOCAL_DIRECTORY}/scripts
-  #   - GMS_HYPERVISOR_SCRIPTS   ${GMS_SCRIPTS}/hypervisor
+  #   - GENOMAC_SYSTEM_LOCAL_DIRECTORY   ~/.genomac-system
+  #   - GMS_SCRIPTS                      ${GENOMAC_SYSTEM_LOCAL_DIRECTORY}/scripts
+  #   - GMS_HYPERVISOR_SCRIPTS           ${GMS_SCRIPTS}/hypervisor
   # - Functions
   #   - export_and_report
   #   - source_with_report
@@ -27,7 +27,7 @@ function alphahypervisor() {
   #       after helper-misc.sh is sourced
 
   # WARNING: The below path for initial_initialization_script is hard-wired (without reference
-  # to the appropriate environment variable (GMS_LOCAL_DIRECTORY) and needs to be monitored for 
+  # to the appropriate environment variable (GENOMAC_SYSTEM_LOCAL_DIRECTORY) and needs to be monitored for 
   # continued appropriateness.
   initial_initialization_script="$HOME/.genomac-system/scripts/0_initialize_me_first.sh"
   if source "$initial_initialization_script" 2>/dev/null; then
@@ -38,8 +38,8 @@ function alphahypervisor() {
   fi
 
   ############### Update clone
-  # Updates the clone of GenoMac-system that is assumed to reside at GMS_LOCAL_DIRECTORY
-  echo "Updating local clone of GenoMac-system at ${GMS_LOCAL_DIRECTORY}"
+  # Updates the clone of GenoMac-system that is assumed to reside at GENOMAC_SYSTEM_LOCAL_DIRECTORY
+  echo "Updating local clone of GenoMac-system at ${GENOMAC_SYSTEM_LOCAL_DIRECTORY}"
   update_genomac_system_repo
 
   ############### Finish initializations
@@ -62,11 +62,11 @@ function alphahypervisor() {
 function update_genomac_system_repo() {
   # NOTE: The immediately below check for the existence of this repo is comically useless:
   #       if the repo has not been cloned, this script itself would not exist.
-  if [[ ! -d ${GMS_LOCAL_DIRECTORY} || -z "$(ls -A ${GMS_LOCAL_DIRECTORY} 2>/dev/null)" ]]; then
-    echo "You must clone the GenoMac-system repo to ${GMS_LOCAL_DIRECTORY} before running the Hypervisor"
+  if [[ ! -d ${GENOMAC_SYSTEM_LOCAL_DIRECTORY} || -z "$(ls -A ${GENOMAC_SYSTEM_LOCAL_DIRECTORY} 2>/dev/null)" ]]; then
+    echo "You must clone the GenoMac-system repo to ${GENOMAC_SYSTEM_LOCAL_DIRECTORY} before running the Hypervisor"
     return 1
   fi
   
-  cd "${GMS_LOCAL_DIRECTORY}"
+  cd "${GENOMAC_SYSTEM_LOCAL_DIRECTORY}"
   git pull --recurse-submodules origin main
 }
