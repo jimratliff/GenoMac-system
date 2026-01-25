0123c03d6f8ddf1dc77bbd5461562c794d94f3f1
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-24 19:20:28 -0800

    Update run_hypervisor.sh

--- scripts/run_hypervisor.sh
+++ scripts/run_hypervisor.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env zs
 
+# Runs the function alphahypervisor, which is the entry point for the Hypervisor process.
+
 # Fail early on unset variables or command failure
 set -euo pipefail
 
 source "${HOME}/.genomac-system/scripts/0_initialize_me_first.sh
+
+# WARNING: The below path for initial_initialization_script is hard-wired (without reference
+# to the appropriate environment variable (GENOMAC_SYSTEM_LOCAL_DIRECTORY) and needs to be monitored for 
+# continued appropriateness.
+
+local initial_initialization_script
+initial_initialization_script="$HOME/.genomac-system/scripts/0_initialize_me_first.sh"
+
+if source "$initial_initialization_script" 2>/dev/null; then
+  echo "Sourced: $initial_initialization_script"
+else
+  echo "Failed to source: $initial_initialization_script"
+  return 1
+fi
 
 # Source required files
 source "${GMS_HYPERVISOR_SCRIPTS}/alphahypervisor.sh"
