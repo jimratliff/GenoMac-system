49b305b144685038bf27db3f0f803a8642e1ff15
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-24 19:58:02 -0800

    Update run_hypervisor.sh

--- scripts/run_hypervisor.sh
+++ scripts/run_hypervisor.sh
@@ -5,8 +5,6 @@
 # Fail early on unset variables or command failure
 set -euo pipefail
 
-source "${HOME}/.genomac-system/scripts/0_initialize_me_first.sh
-
 # WARNING: The below path for initial_initialization_script is hard-wired (without reference
 # to the appropriate environment variable (GENOMAC_SYSTEM_LOCAL_DIRECTORY) and needs to be monitored for 
 # continued appropriateness.
