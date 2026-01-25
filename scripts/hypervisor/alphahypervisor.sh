7d3d28ef1cdc4997b4f2b47a6f425e57477ec94a
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-24 19:21:15 -0800

    Update alphahypervisor.sh

--- scripts/hypervisor/alphahypervisor.sh
+++ scripts/hypervisor/alphahypervisor.sh
@@ -8,34 +8,13 @@ function alphahypervisor() {
   #
   # This is the function called first, directly, and immediately by `make run-hypervisor`.
   #
-  # It assumes that GenoMac-system has been cloned locally to GENOMAC_SYSTEM_LOCAL_DIRECTORY (~/.genomac-system).
-  # It is *not* necessary to update the clone before running this function, because this function updates the clone.
+  # It assumes that:
+  # - GenoMac-system has been cloned locally to GENOMAC_SYSTEM_LOCAL_DIRECTORY (~/.genomac-system).
+  #   - It is *not* necessary to update the clone before running this function, because this function
+  #     updates the clone.
+  # - scripts/0_initialize_me_first.sh has been sourced
   
-
   echo "Inside alphahypervisor"
-
-  ############### Bootstrapping: Source initial initialization script
-  # This will make available:
-  # - Environment variables
-  #   - GENOMAC_SYSTEM_LOCAL_DIRECTORY   ~/.genomac-system
-  #   - GMS_SCRIPTS                      ${GENOMAC_SYSTEM_LOCAL_DIRECTORY}/scripts
-  #   - GMS_HYPERVISOR_SCRIPTS           ${GMS_SCRIPTS}/hypervisor
-  # - Functions
-  #   - export_and_report
-  #   - source_with_report
-  #     - This will be disfavored relative to safe_source, once safe_source becomes available
-  #       after helper-misc.sh is sourced
-
-  # WARNING: The below path for initial_initialization_script is hard-wired (without reference
-  # to the appropriate environment variable (GENOMAC_SYSTEM_LOCAL_DIRECTORY) and needs to be monitored for 
-  # continued appropriateness.
-  initial_initialization_script="$HOME/.genomac-system/scripts/0_initialize_me_first.sh"
-  if source "$initial_initialization_script" 2>/dev/null; then
-    echo "Sourced: $initial_initialization_script"
-  else
-    echo "Failed to source: $initial_initialization_script"
-    return 1
-  fi
 
   ############### Update clone
   # Updates the clone of GenoMac-system that is assumed to reside at GENOMAC_SYSTEM_LOCAL_DIRECTORY
