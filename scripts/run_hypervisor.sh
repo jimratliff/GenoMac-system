783e347134820691a39c4a45a2f4eed6b852a037
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-24 20:21:08 -0800

    Update run_hypervisor.sh

--- scripts/run_hypervisor.sh
+++ scripts/run_hypervisor.sh
@@ -6,7 +6,6 @@
 set -euo pipefail
 
 initial_initialization_script="$HOME/.genomac-system/scripts/0_initialize_me_first.sh"
-
 if source "$initial_initialization_script" 2>/dev/null; then
   echo "Sourced: $initial_initialization_script"
 else
@@ -14,12 +13,8 @@ else
   return 1
 fi
 
-secondary_initialization_script="${GMS_SCRIPTS}/0_initialize_me_second.sh"
-source_with_report "${secondary_initialization_script}"
-
 # Source required files
 source "${GMS_HYPERVISOR_SCRIPTS}/hypervisor.sh"
-
 
 function main() {
   hypervisor
