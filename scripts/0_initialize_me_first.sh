5ecff5760505f4c0479ebc0b097130bbe386cf15
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-25 10:05:35 -0800

    Update 0_initialize_me_first.sh

--- scripts/0_initialize_me_first.sh
+++ scripts/0_initialize_me_first.sh
@@ -6,45 +6,23 @@
 # Fail early on unset variables or command failure
 set -euo pipefail
 
-function initial_initialization() {
+local GENOMAC_SYSTEM_SCRIPTS
+local GENOMAC_SYSTEM_ROOT
+local GENOMAC_SHARED_ROOT_RELATIVE_TO_GENOMAC_SYSTEM
 
-  function source_with_report() {
-    # Ensures that an error is raised if a `source` of the file in the supplied argument fails.
-    #
-    # Defining this function here solves a chicken-or-egg problem: We’d like to use the helper 
-    # safe_source(), but it hasn’t been sourced yet. The current function is not quite as full functional 
-    # but will do for the initial sourcing of helpers.
-    local file="$1"
-    if source "$file"; then
-      echo "Sourced: $file"
-    else
-      echo "Failed to source: $file"
-      return 1
-    fi
-  }
+this_script_path="${0:A}"                       # ~/.genomac-system/scripts/0_initialize_me_first.sh
+GENOMAC_SYSTEM_SCRIPTS="${this_script_path:h}"  # ~/.genomac-system/scripts
+GENOMAC_SYSTEM_ROOT="${GMS_SCRIPTS:h}"          # ~/.genomac-system
+GENOMAC_SHARED_ROOT_RELATIVE_TO_GENOMAC_SYSTEM="${GENOMAC_SYSTEM_ROOT}/external/genomac-shared"
 
-  this_script_path="${0:A}"                       # ~/.genomac-system/scripts/0_initialize_me_first.sh
-  GENOMAC_SYSTEM_SCRIPTS="${this_script_path:h}"  # ~/.genomac-system/scripts
-  GENOMAC_SYSTEM_ROOT="${GMS_SCRIPTS:h}"          # ~/.genomac-system
-  GENOMAC_SHARED_ROOT_RELATIVE_TO_GENOMAC_SYSTEM="${GENOMAC_SYSTEM_ROOT}/external/genomac-shared"
-  
-  # Source helpers and environment variables from GenoMac-shared, which appears as a submodule of GenoMac-system
-  HELPERS_FROM_GENOMAC_SHARED="${GENOMAC_SYSTEM_SCRIPTS:h}/external/genomac-shared/scripts"  # external/genomac-shared/scripts
-  # Source the master-helper script from GenoMac-shared submodule, which sources all other components
-  # from GenoMac-shared
-  source_with_report "${HELPERS_FROM_GENOMAC_SHARED}/helpers.sh"
-  
-  # Source repo-specific environment-variables script
-  source_with_report "${GMS_SCRIPTS}/assign_system_environment_variables.sh"
+# Source helpers and environment variables from GenoMac-shared, which appears as a submodule of GenoMac-system
+HELPERS_FROM_GENOMAC_SHARED="${GENOMAC_SYSTEM_SCRIPTS:h}/external/genomac-shared/scripts"  # external/genomac-shared/scripts
+# Source the master-helper script from GenoMac-shared submodule, which sources all other components
+# from GenoMac-shared
+local master_helper_script
+master_helper_script="${HELPERS_FROM_GENOMAC_SHARED}/helpers.sh"
+echo "Source ${master_helper_script}"
+source "${master_helper_script}"
 
-}
-
-
-
-
-
-
-
-
-
-
+# Source repo-specific environment-variables script
+source_with_report "${GMS_SCRIPTS}/assign_system_environment_variables.sh"
