a868a0db8d9eabf8b33fdadcb7ea1c7a7f9ce8be
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-27 19:48:05 -0800

    Update subdermis.sh

--- scripts/hypervisor/subdermis.sh
+++ scripts/hypervisor/subdermis.sh
@@ -57,7 +57,6 @@ function subdermis() {
   conditionally_install_resources_systemwide  # scripts/installations/of_resources/install_resources.sh
   conditionally_implement_systemwide_settings # scripts/settings/implement_systemwide_settings.sh
   conditionally_clone_genomac_user            # scripts/user_scope/clone_genomac_user_repo.sh
-  delete_all_system_SESH_states               # scripts/helpers-state.sh
 
   
   # TODO: Un-comment-out the below 'figlet' line after GenoMac-system is refactored so that it works
