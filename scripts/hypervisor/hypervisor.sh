befea43f6e71b16961667c733a8d3b0cae9b04b1
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-30 14:56:22 -0800

    Update hypervisor.sh

--- scripts/hypervisor/hypervisor.sh
+++ scripts/hypervisor/hypervisor.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-safe_source "${GMU_HYPERVISOR_SCRIPTS}/subdermis.sh"
+safe_source "${GMS_HYPERVISOR_SCRIPTS}/subdermis.sh"
 
 function hypervisor() {
   # The outermost “dermal” layer of hypervisory supervison (the dermis). Ensures the 
@@ -27,17 +27,17 @@ function hypervisor() {
   keep_sudo_alive
 
   if ! test_genomac_system_state "SESH_REPO_HAS_BEEN_TESTED_FOR_CHANGES"; then
-  	report_action_taken "Testing remote copy of ${GENOMAC_USER_REPO_NAME} for changes"
-  	if local_clone_was_updated_from_remote "$GENOMAC_USER_LOCAL_DIRECTORY"; then
+  	report_action_taken "Testing remote copy of ${GENOMAC_SYSTEM_REPO_NAME} for changes"
+  	if local_clone_was_updated_from_remote "$GENOMAC_SYSTEM_LOCAL_DIRECTORY"; then
 	  # The local clone was found to be behind the remote; local clone updated, and then
 	  # this script is re-executed.
-      set_genomac_user_state "SESH_REPO_HAS_BEEN_TESTED_FOR_CHANGES"
+      set_genomac_system_state "SESH_REPO_HAS_BEEN_TESTED_FOR_CHANGES"
   	  report_action_taken "Re-execute Hypervisor using updated repo code"
   	  report_end_phase_standard
   	  exec "$0"
 	else
-      set_genomac_user_state "SESH_REPO_HAS_BEEN_TESTED_FOR_CHANGES"
-	  report "Local clone of ${GENOMAC_USER_REPO_NAME} was up to date"
+      set_genomac_system_state "SESH_REPO_HAS_BEEN_TESTED_FOR_CHANGES"
+	  report "Local clone of ${GENOMAC_SYSTEM_REPO_NAME} was up to date"
 	fi
   else
 	report_action_taken "Skipping test for changes to repo, because this has already been tested this session."
@@ -47,7 +47,7 @@ function hypervisor() {
   subdermis
 
   # Reset SESH states for next session
-  delete_all_user_SESH_states
+  delete_all_system_SESH_states
 
   report_end_phase_standard
 }
