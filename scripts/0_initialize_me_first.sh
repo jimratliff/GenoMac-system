8bd24e794e0ab6ed5900360517b7f7492472a872
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-30 19:23:06 -0800

    Update 0_initialize_me_first.sh

--- scripts/0_initialize_me_first.sh
+++ scripts/0_initialize_me_first.sh
@@ -15,10 +15,10 @@ set -euo pipefail
 
 # Get path of THIS script, even when sourced
 # Explanation:
-# %x — zsh prompt escape meaning "path of the script being sourced"
-# ${(%):-%x} — trick to evaluate a prompt escape outside a prompt (the (%) flag)
-# ${...:A} — resolve to absolute path
-# So ${${(%):-%x}:A} means "the absolute path of the file currently being sourced."
+#   %x — zsh prompt escape meaning "path of the script being sourced"
+#   ${(%):-%x} — trick to evaluate a prompt escape outside a prompt (the (%) flag)
+#   ${...:A} — resolve to absolute path
+#   So ${${(%):-%x}:A} means "the absolute path of the file currently being sourced."
 this_script_path="${${(%):-%x}:A}"                  # ~/.genomac-system/scripts/0_initialize_me_first.sh
 
 # NOTE: The following are NOT exported. They are defined/calculated here only for the purpose
@@ -26,6 +26,7 @@ this_script_path="${${(%):-%x}:A}"                  # ~/.genomac-system/scripts/
 GENOMAC_SYSTEM_SCRIPTS="${this_script_path:h}"      # ~/.genomac-system/scripts
 GENOMAC_SYSTEM_ROOT="${GENOMAC_SYSTEM_SCRIPTS:h}"   # ~/.genomac-system
 GENOMAC_SHARED_ROOT_RELATIVE_TO_GENOMAC_SYSTEM="${GENOMAC_SYSTEM_ROOT}/external/genomac-shared" # ~/.genomac-system/external/genomac-shared
+HELPERS_FROM_GENOMAC_SHARED="${GENOMAC_SHARED_ROOT_RELATIVE_TO_GENOMAC_SYSTEM}/scripts"  # external/genomac-shared/scripts
 
 echo "Paths determined in 0_initialize_me_first.sh:"
 echo "• this_script_path: ${this_script_path}"
@@ -35,7 +36,6 @@ echo "• GENOMAC_SHARED_ROOT_RELATIVE_TO_GENOMAC_SYSTEM: ${GENOMAC_SHARED_ROOT_
 
 # Source the master-helper script from GenoMac-shared submodule, which sources helpers
 # and environment variables from GenoMac-shared
-HELPERS_FROM_GENOMAC_SHARED="${GENOMAC_SHARED_ROOT_RELATIVE_TO_GENOMAC_SYSTEM}/scripts"  # external/genomac-shared/scripts
 master_helper_script="${HELPERS_FROM_GENOMAC_SHARED}/helpers.sh"
 
 echo "Source ${master_helper_script}"
