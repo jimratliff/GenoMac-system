2b9352d70e911c889565df5027faae72260f8050
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-01-31 13:57:26 -0800

    Update hypervisor.sh

--- scripts/hypervisor/hypervisor.sh
+++ scripts/hypervisor/hypervisor.sh
@@ -34,7 +34,7 @@ function hypervisor() {
   	if local_clone_was_updated_from_remote "$GENOMAC_SYSTEM_LOCAL_DIRECTORY"; then
       # The local clone was found to be behind the remote; local clone updated, and then
       # this script is re-executed.
-      set_genomac_system_state "SESH_REPO_HAS_BEEN_TESTED_FOR_CHANGES"
+      set_genomac_system_state "$SESH_REPO_HAS_BEEN_TESTED_FOR_CHANGES"
       report_action_taken "Re-execute Hypervisor using updated repo code"
       report_end_phase_standard
       exec "$0"
