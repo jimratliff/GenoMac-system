773d964ce43fa3fb6fe812e6832a566fdde3ed4d
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-04-28 21:02:03 -0700

    Update spawn.sh

--- scripts/spawn/spawn.sh
+++ scripts/spawn/spawn.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 safe_source "${GMS_USER_SPAWNING_SCRIPTS}/spawn-helpers.sh"
 safe_source "${GMS_USER_SPAWNING_SCRIPTS}/spawn-addUser.sh"
 safe_source "${GMS_USER_SPAWNING_SCRIPTS}/spawn-create-volume.sh"
+safe_source "${GMS_USER_SPAWNING_SCRIPTS}/spawn-interactive.sh"
 
 # Global associative arrays to be populated from item ONEPASSWORD_ITEM_NAME_USER_SPAWN_CONFIG
 # of 1Password vault ONEPASSWORD_VAULT_FOR_GENOMAC_USER_CREATION
