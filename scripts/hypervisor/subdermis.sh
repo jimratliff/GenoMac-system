570b4f19b6ad75e4f01cc68edf17c5cfaae04c73
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-07 18:45:18 -0700

    Update subdermis.sh

--- scripts/hypervisor/subdermis.sh
+++ scripts/hypervisor/subdermis.sh
@@ -8,6 +8,7 @@ function subdermis() {
   safe_source "${GMS_HOMEBREW_INSTALL_SCRIPTS}/adjust_path_for_homebrew.sh"
   safe_source "${GMS_HOMEBREW_INSTALL_SCRIPTS}/install_via_homebrew.sh"
   safe_source "${GMS_HOMEBREW_INSTALL_SCRIPTS}/interactive_sign_into_MAS.sh"
+  safe_source "${GMS_INSTALL_SCRIPTS}/helpers_for_installations.sh"
   safe_source "${GMS_NON_HOMEBREW_INSTALL_SCRIPTS}/install_non_homebrew_apps.sh"
   safe_source "${GMS_RESOURCE_INSTALL_SCRIPTS}/install_resources.sh"
   safe_source "${GMS_SETTINGS_SCRIPTS}/implement_systemwide_settings.sh"
