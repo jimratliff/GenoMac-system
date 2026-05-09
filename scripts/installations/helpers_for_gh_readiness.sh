24bb72117a4d38bc4c3351f77a841d57a54fce3b
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-09 11:08:04 -0700

    Update helpers_for_gh_readiness.sh

--- scripts/installations/helpers_for_gh_readiness.sh
+++ scripts/installations/helpers_for_gh_readiness.sh
@@ -12,39 +12,42 @@ function conditionally_test_for_gh_availability() {
 function conditionally_test_for_gh_authentication() {
   report_start_phase_standard
   run_if_system_has_not_done \
-    "$SESH_GH_ATHENTICATION_HAS_BEEN_ASCERTAINED" \
+    "$SESH_GH_AUTHENTICATION_HAS_BEEN_ASCERTAINED" \
     set_state_based_on_gh_authentication \
     "Skipping testing gh authentication, because its authentication has been ascertained earlier this session."
   report_end_phase_standard
 }
 
 function set_state_based_on_gh_availability() {
-  determine_system_state_based_on_value "$SESH_GH_IS_AVAILABLE" "$(is_gh_available)"
+  determine_system_state_based_on_value "$SESH_GH_IS_AVAILABLE" "$(gh_availability_indicator)"
 }
 
 function set_state_based_on_gh_authentication() {
-  determine_system_state_based_on_value "$SESH_GH_IS_AUTHENTICATED" "$(is_gh_authenticated)"
+  determine_system_state_based_on_value "$SESH_GH_IS_AUTHENTICATED" "$(gh_authentication_indicator)"
 }
 
-function is_gh_available(){
-  # Returns 0 if gh is available (though not necessarily authenticated); return 1 otherwise.
+function gh_availability_indicator(){
+  # Returns 0 if gh is available (though not necessarily authenticated); returns 1 otherwise.
   report_start_phase_standard
   if ! command -v gh >/dev/null 2>&1; then
-    report_end_phase_standard
     print -- "1"
+  else
+    print -- "0"
   fi
   report_end_phase_standard
-  print -- "0"
+  return 0
 }
   
-function is_gh_authenticated() {
-  # Returns 0 if gh is authenticated; return 1 otherwise.
+function gh_authentication_indicator() {
+  # Returns 0 if gh is authenticated; returns 1 otherwise.
   report_start_phase_standard
   if ! gh auth status >/dev/null 2>&1; then
     print -- "1"
+  else
+    print -- "0"
   fi
   report_end_phase_standard
-  print -- "0"
+  return 0
 }
 
 
