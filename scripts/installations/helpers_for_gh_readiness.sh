585057ef10dc8a5800e3229d838ae8a3f9e0ba6f
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-09 12:31:29 -0700

    Update helpers_for_gh_readiness.sh

--- scripts/installations/helpers_for_gh_readiness.sh
+++ scripts/installations/helpers_for_gh_readiness.sh
@@ -1,5 +1,20 @@
 #!/bin/zsh
 
+function gh_is_authenticated() {
+  # Returns 0 if gh is authenticated; otherwise, returns 1
+  local return_value
+  report_start_phase_standard
+  conditionally_test_for_gh_availability
+  conditionally_test_for_gh_authentication
+  if test_genomac_system_state "$SESH_GH_IS_AUTHENTICATED"; then
+    return_value=0
+  else
+    return_value=1
+  fi
+  report_end_phase_standard
+  return "$return_value"
+}
+
 function conditionally_test_for_gh_availability() {
   report_start_phase_standard
   run_if_system_has_not_done \
@@ -30,6 +45,7 @@ function gh_availability_indicator(){
   # Prints "0" to stdout if gh is available (though not necessarily authenticated); prints "1" otherwise.
   report_start_phase_standard
   if ! command -v gh >/dev/null 2>&1; then
+    report_warning "gh unavailable. This is unexpected, because gh should be installed by Homebrew."
     print -- "1"
   else
     print -- "0"
@@ -42,6 +58,7 @@ function gh_authentication_indicator() {
   # Prints "0" to stdout if gh is authenticated; prints "1" otherwise.
   report_start_phase_standard
   if ! gh auth status >/dev/null 2>&1; then
+    report_warning "gh isn’t authenticated. This is normal if the user hasn’t been set up to authenticate with GitHub yet."
     print -- "1"
   else
     print -- "0"
