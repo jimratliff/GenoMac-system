4aa0437d444facef49d0d88e5fd7dd4b4897b1e7
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-22 21:20:39 -0700

    Update spawn-volume-creation-helpers.sh

--- scripts/spawn/spawn-volume-creation-helpers.sh
+++ scripts/spawn/spawn-volume-creation-helpers.sh
@@ -20,7 +20,8 @@ function _test_volume_1Password_key_state_was_found_without_mismatch(){
   # Tests whether exactly one state exists for the desired volume/1Password key.
   #
   # Returns:
-  #   0 if exactly one matching state exists
+  #   0 if exactly one matching state exists and its 1Password key matches the desired key
+  #     (If the 1Password key of the existing state is different, exits/bombs)
   #   1 if no matching state exists
   #   exits/bombs if multiple matching states exist
   #     Multiple matching states implies that the same volume is assigned multiple
@@ -33,28 +34,48 @@ function _test_volume_1Password_key_state_was_found_without_mismatch(){
   local op_item_key="${2:?missing/empty op_item_key}"
   local state_string_prefix="${3:?missing/empty state_string_prefix}"
   local state_string_prefix_with_volume_name
+  local desired_state_string
+  local failure_message
+  local matching_state_string
   local -a matching_state_strings
 
-  state_string_prefix_with_volume_name=\
-"${state_string_prefix}${GENOMAC_STATE_STRING_DELIMITER_A}${volume_name}${GENOMAC_STATE_STRING_DELIMITER_B}${op_item_key}"
+  state_string_prefix_with_volume_name="${state_string_prefix}${GENOMAC_STATE_STRING_DELIMITER_A}${volume_name}${GENOMAC_STATE_STRING_DELIMITER_B}"
 
-  _state_strings_with_prefix "$state_string_prefix_with_volume_name" "$GENOMAC_SCOPE_SYSTEM" || return 1
+  # Collects all state strings for this volume (with $state_string_prefix)
+  _state_strings_with_prefix "$state_string_prefix_with_volume_name" "$GENOMAC_SCOPE_SYSTEM" || exit 70
   matching_state_strings=("${reply[@]}")
 
   case "${#matching_state_strings[@]}" in
     0)
+      # No existing state for this volume (with $state_string_prefix)
       report_end_phase_standard
       return 1
       ;;
-
+  
     1)
-      report_end_phase_standard
-      return 0
+      # Exactly one existing state for this volume (with $state_string_prefix)
+      # Test that the 1Password key of the existing state matches the desired key
+      desired_state_string="${state_string_prefix_with_volume_name}${op_item_key}"
+      if [[ "${matching_state_strings[1]}" == "${desired_state_string}" ]]; then
+        report_end_phase_standard
+        return 0
+      fi
+      report_fail \
+        "State exists for volume “${volume_name}”, but with mismatched 1Password item key.${NEWLINE}"\
+        "Expected state string: ${desired_state_string}${NEWLINE}"\
+        "Found state string: ${matching_state_strings[1]}"
+      exit 70
       ;;
-
+  
     *)
-      report_fail "Multiple matching states found for volume “${volume_name}” and 1Password item key “${op_item_key}”."
-      printf '  %s\n' "${matching_state_strings[@]}" >&2
+      # Two or more existing states for this volume (with $state_string_prefix)
+      # This implies more than one encryption passphrase for the volume, which is a conflict.
+      failure_message="Multiple states found for prefix: ${state_string_prefix_with_volume_name}${NEWLINE}"
+      failure_message+="Matching state strings:"
+      for matching_state_string in "${matching_state_strings[@]}"; do
+        failure_message+="${NEWLINE}  ${matching_state_string}"
+      done
+      report_fail "$failure_message"
       exit 70
       ;;
   esac
