90388d82b78fb3deb1236043ec1a8fab601056d7
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-24 09:24:42 -0700

    Update spawn-state-helpers.sh

--- scripts/spawn/spawn-state-helpers.sh
+++ scripts/spawn/spawn-state-helpers.sh
@@ -2,7 +2,7 @@
 
 function get_1Password_key_from_delimited_state_string(){
   # Get the 1Password item key from a delimited volume-1Pkey state string
-  # See _construct_state_string_for_volume_1password_key() for background
+  # See construct_state_string_for_volume_1password_key() for background
   report_start_phase_standard
   local state_string="$1"
   local op_key
