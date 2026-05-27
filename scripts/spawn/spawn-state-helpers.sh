342a28de5f60f3703980780ae2b666d0d73f2d95
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-26 20:10:59 -0700

    Update spawn-state-helpers.sh

--- scripts/spawn/spawn-state-helpers.sh
+++ scripts/spawn/spawn-state-helpers.sh
@@ -15,6 +15,23 @@ function mark_user_as_created(){
   report_end_phase_standard
 }
 
+function set_system_states_for_user_attributes(){
+  # Sets a system-scoped state for each attribute of user, whose user_spec_json is supplied as $1.
+  report_start_phase_standard
+  
+  local user_spec_json="$1"
+
+  short_name="$(get_short_name_from_user_spec_json "$user_spec_json")" || return 1
+
+  local attribute_name
+  while IFS= read -r attribute_name; do
+    report_adjust_setting "Set system-scoped state $GENOMAC_STATE_USER_ATTRIBUTE_PREFIX for user $short_name with attribute $attribute_name"
+    set_system_state_for_user_attribute "$short_name" "$attribute_name"
+  done < <(attribute_names_from_user_spec_json "$user_spec_json")
+
+  report_end_phase_standard
+}
+
 function get_1Password_key_from_delimited_state_string_DEPRECATED(){
   # Get the 1Password item key from a delimited volume-1Pkey state string
   # See construct_state_string_for_volume_1password_key_pending_creation() for background
