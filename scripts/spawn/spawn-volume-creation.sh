21227caaa21346ba3064bae59f032f7be46fec79
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-28 10:50:59 -0700

    Update spawn-volume-creation.sh

--- scripts/spawn/spawn-volume-creation.sh
+++ scripts/spawn/spawn-volume-creation.sh
@@ -3,6 +3,19 @@
 function conditionally_interactive_create_volumes_for_user_home_directories(){
   # Template for a Zsh function in Project GenoMac
   report_start_phase_standard
+
+  local -a pending_volume_state_strings
+  collect_state_strings_for_volumes_pending_creation
+  pending_volume_state_strings=("${reply[@]}")
+
+  if (( ! ${#pending_volume_state_strings[@]} )); then
+    report "There are no volumes pending creation. Moving on…"
+    report_end_phase_standard
+    return 0
+  fi
+
+  
+  
   report_end_phase_standard
 }
 
