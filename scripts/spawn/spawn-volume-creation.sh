286471f7a9fe662772ea97467d717a312e9a2fa2
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-28 15:42:41 -0700

    Update spawn-volume-creation.sh

--- scripts/spawn/spawn-volume-creation.sh
+++ scripts/spawn/spawn-volume-creation.sh
@@ -8,28 +8,18 @@ function conditionally_interactive_create_volumes_for_user_home_directories(){
   collect_state_strings_for_volumes_pending_creation
   pending_volume_state_strings=("${reply[@]}")
 
-  if (( ! ${#pending_volume_state_strings[@]} )); then
+  local number_of_pending_volumes
+  number_of_pending_volumes=${#pending_volume_state_strings[@]}
+
+  if (( ! $number_of_pending_volumes )); then
     report "There are no volumes pending creation. Moving on…"
     report_end_phase_standard
     return 0
   fi
+  print report "There is/are $number_of_pending_volumes volume(s) pending creation."
 
-  
-  
+
   report_end_phase_standard
 }
 
-function collect_state_strings_for_volumes_pending_creation(){
-  # Sets reply to an array of state strings of system-scoped states that assert
-  # that a volume name is pending creation.
 
-  report_start_phase_standard
-  local pattern
-  local -a matching_state_paths
-  
-  pattern="${GMS_STATE_VOLUME_IS_PENDING_PREFIX}${GENOMAC_STATE_STRING_DELIMITER_A}"
-  matching_state_paths=("${GENOMAC_SYSTEM_LOCAL_STATE_DIRECTORY}"/"${pattern}"*."${GENOMAC_STATE_FILE_EXTENSION}"(N:t:r))
-  reply=("${matching_state_paths[@]}")
-  
-  report_end_phase_standard
-}
