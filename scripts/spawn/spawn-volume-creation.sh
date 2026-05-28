566f5a4851d94a346448519a04078c9717452bb6
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-28 16:14:48 -0700

    Update spawn-volume-creation.sh

--- scripts/spawn/spawn-volume-creation.sh
+++ scripts/spawn/spawn-volume-creation.sh
@@ -18,8 +18,40 @@ function conditionally_interactive_create_volumes_for_user_home_directories(){
   fi
   print report "There is/are $number_of_pending_volumes volume(s) pending creation."
 
+  construct_map_from_volume_key_to_open_item_key_from_pending_creation_state_strings
+
 
   report_end_phase_standard
 }
 
+function construct_map_from_volume_key_to_open_item_key_from_pending_creation_state_strings(){
+  # Returns associative array from array of volumes-pending-creation state strings, where
+  # the associative array maps volume_name to op_item_key.
+  #
+  # If multiple state strings share a common volume_name, generate fatal error.
+  
+  report_start_phase_standard
+  local pending_volume_state_strings
+  pending_volume_state_strings="$1"
 
+  local -A op_item_key_from_volume_name
+  local state_string
+  local volume_name
+  local op_item_key
+  local error_string
+
+  for state_string in "${pending_volume_state_strings[@]}"; do
+    volume_name=$(volume_name_from_pending_volume_state_string "$state_string")
+    op_item_key=$(op_item_key_from_pending_volume_state_string "$state_string")
+
+    if [[ -v 'op_item_key_from_volume_name[$volume_name]' ]]; then
+      report_fail "Multiple pending-creation states for volume ${volume_name}"
+      return 1
+    fi
+
+    op_item_key_from_volume_name[$volume_name]="$op_item_key"
+  done
+  reply=("${op_item_key_from_volume_name[@]}")
+  
+  report_end_phase_standard
+}
