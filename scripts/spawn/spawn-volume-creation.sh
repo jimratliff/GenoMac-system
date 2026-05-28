3acba35baed5ded3211edce49a6372ed3d058c16
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-28 10:24:00 -0700

    Create spawn-volume-creation.sh

---
+++ scripts/spawn/spawn-volume-creation.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+function conditionally_interactive_create_volumes_for_user_home_directories(){
+  # Template for a Zsh function in Project GenoMac
+  report_start_phase_standard
+  report_end_phase_standard
+}
+
+function collect_state_strings_for_volumes_pending_creation(){
+  # Sets reply to an array of state strings of system-scoped states that assert
+  # that a volume name is pending creation.
+
+  report_start_phase_standard
+  local pattern
+  local -a matching_state_paths
+  
+  pattern="${GMS_STATE_VOLUME_IS_PENDING_PREFIX}${GENOMAC_STATE_STRING_DELIMITER_A}"
+  matching_state_paths=("${GENOMAC_SYSTEM_LOCAL_STATE_DIRECTORY}"/"${pattern}"*."${GENOMAC_STATE_FILE_EXTENSION}"(N:t:r))
+  reply=("${matching_state_paths[@]}")
+  
+  report_end_phase_standard
+}
