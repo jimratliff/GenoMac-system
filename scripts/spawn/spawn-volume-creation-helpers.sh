885f6fbb220aaa102f24931cdf0ec46e56465e2a
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-28 18:09:17 -0700

    Update spawn-volume-creation-helpers.sh

--- scripts/spawn/spawn-volume-creation-helpers.sh
+++ scripts/spawn/spawn-volume-creation-helpers.sh
@@ -26,3 +26,15 @@ function volume_name_is_mounted() {
   mount | grep -Fq " on ${mount_point} "
   report_end_phase_standard
 }
+
+function volume_name_is_startup_volume_signifier() {
+  # Returns 0 if supplied volume name is $STARTUP_VOLUME_SIGNIFIER, otherwise returns 1.
+  report_start_phase_standard
+  local volume_name="${1:?missing volume_name}"
+  if [[ "$volume_name" == "$STARTUP_VOLUME_SIGNIFIER" ]]; then
+    report_end_phase_standard
+    return 0
+  fi
+  report_end_phase_standard
+  return 1
+}
