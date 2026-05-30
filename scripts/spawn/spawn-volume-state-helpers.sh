9395bc15a1beb9f5f4237beaf79bb707a7503d55
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-05-30 11:16:26 -0700

    Update spawn-volume-state-helpers.sh

--- scripts/spawn/spawn-volume-state-helpers.sh
+++ scripts/spawn/spawn-volume-state-helpers.sh
@@ -256,79 +256,7 @@ function conditionally_mark_volume_is_necessary(){
 
 
 
-function test_volume_1Password_key_state_was_found_without_mismatch(){
-  # Tests whether exactly one volume-creation-is-pending state exists for the desired volume.
-  # If so, crashes if the existing 1Password key doesn’t match the desired 1Password key supplied.
-  #
-  # Parameters:
-  #   $1: volume name
-  #   $2: 1Password item key for the desired passphrase for this volume
-  #
-  # Returns:
-  #   0 if exactly one matching state exists and its 1Password key matches the desired key
-  #     (If the 1Password key of the existing state is different, exits/bombs)
-  #   1 if no state matches the volume/prefix
-  #
-  #   Exits/bombs with exit code 70 if:
-  #   - multiple matching states exist
-  #       Multiple matching states implies that the same volume is assigned multiple
-  #       1Password item keys, which is a conflict, because a volume can have only a
-  #       single encryption passphrase.
-  #   - one state matches the volume/prefix, but its 1Password key is different from the
-  #     supplied as the desired 1Password key
 
-  report_start_phase_standard
-
-  local volume_name="${1:?missing/empty volume_name}"
-  local op_item_key="${2:?missing/empty op_item_key}"
-  local state_string_prefix="$GMS_STATE_VOLUME_IS_NECESSARY_PREFIX"
-  local volume_search_prefix
-  local desired_state_string
-  local failure_message
-  local matching_state_string
-  local -a matching_state_strings
-
-  volume_search_prefix="$(construct_state_string_for_volume_1password_key_pending_creation --volume-only "${volume_name}")"
-
-  # Collects all state strings for this volume (with $state_string_prefix)
-  _state_strings_with_prefix "$volume_search_prefix" "$GENOMAC_SCOPE_SYSTEM" || exit 70
-  matching_state_strings=("${reply[@]}")
-
-  case "${#matching_state_strings[@]}" in
-    0)
-      # No existing state for this volume (with $state_string_prefix)
-      report_end_phase_standard
-      return 1
-      ;;
-  
-    1)
-      # Exactly one existing state for this volume (with $state_string_prefix)
-      # Test that the 1Password key of the existing state matches the desired key
-      desired_state_string="$(construct_state_string_for_volume_1password_key_pending_creation "${volume_name}" "${op_item_key}")"
-      if [[ "${matching_state_strings[1]}" == "${desired_state_string}" ]]; then
-        report_end_phase_standard
-        return 0
-      fi
-      report_fail \
-        "State exists for volume “${volume_name}”, but with mismatched 1Password item key.${NEWLINE}"\
-        "Expected state string: ${desired_state_string}${NEWLINE}"\
-        "Found state string: ${matching_state_strings[1]}"
-      exit 70
-      ;;
-  
-    *)
-      # Two or more existing states for this volume (with $state_string_prefix)
-      # This implies more than one encryption passphrase for the volume, which is a conflict.
-      failure_message="Multiple states found for prefix: ${state_string_prefix_with_volume_name}${NEWLINE}"
-      failure_message+="Matching state strings:"
-      for matching_state_string in "${matching_state_strings[@]}"; do
-        failure_message+="${NEWLINE}  ${matching_state_string}"
-      done
-      report_fail "$failure_message"
-      exit 70
-      ;;
-  esac
-}
 
 function construct_state_string_for_volume_1password_key_pending_creation(){
   # Constructs a state string that encodes (a) volume name and (b) 1Password item key.
@@ -450,6 +378,81 @@ function construct_state_string_for_volume_1password_key_pending_creation(){
 
 #############################################
 #        EVERYTHING BELOW THIS IS DEPRECATED
+
+function test_volume_1Password_key_state_was_found_without_mismatch(){
+  # Tests whether exactly one volume-creation-is-pending state exists for the desired volume.
+  # If so, crashes if the existing 1Password key doesn’t match the desired 1Password key supplied.
+  #
+  # Parameters:
+  #   $1: volume name
+  #   $2: 1Password item key for the desired passphrase for this volume
+  #
+  # Returns:
+  #   0 if exactly one matching state exists and its 1Password key matches the desired key
+  #     (If the 1Password key of the existing state is different, exits/bombs)
+  #   1 if no state matches the volume/prefix
+  #
+  #   Exits/bombs with exit code 70 if:
+  #   - multiple matching states exist
+  #       Multiple matching states implies that the same volume is assigned multiple
+  #       1Password item keys, which is a conflict, because a volume can have only a
+  #       single encryption passphrase.
+  #   - one state matches the volume/prefix, but its 1Password key is different from the
+  #     supplied as the desired 1Password key
+
+  report_start_phase_standard
+
+  local volume_name="${1:?missing/empty volume_name}"
+  local op_item_key="${2:?missing/empty op_item_key}"
+  local state_string_prefix="$GMS_STATE_VOLUME_IS_NECESSARY_PREFIX"
+  local volume_search_prefix
+  local desired_state_string
+  local failure_message
+  local matching_state_string
+  local -a matching_state_strings
+
+  volume_search_prefix="$(construct_state_string_for_volume_1password_key_pending_creation --volume-only "${volume_name}")"
+
+  # Collects all state strings for this volume (with $state_string_prefix)
+  _state_strings_with_prefix "$volume_search_prefix" "$GENOMAC_SCOPE_SYSTEM" || exit 70
+  matching_state_strings=("${reply[@]}")
+
+  case "${#matching_state_strings[@]}" in
+    0)
+      # No existing state for this volume (with $state_string_prefix)
+      report_end_phase_standard
+      return 1
+      ;;
+  
+    1)
+      # Exactly one existing state for this volume (with $state_string_prefix)
+      # Test that the 1Password key of the existing state matches the desired key
+      desired_state_string="$(construct_state_string_for_volume_1password_key_pending_creation "${volume_name}" "${op_item_key}")"
+      if [[ "${matching_state_strings[1]}" == "${desired_state_string}" ]]; then
+        report_end_phase_standard
+        return 0
+      fi
+      report_fail \
+        "State exists for volume “${volume_name}”, but with mismatched 1Password item key.${NEWLINE}"\
+        "Expected state string: ${desired_state_string}${NEWLINE}"\
+        "Found state string: ${matching_state_strings[1]}"
+      exit 70
+      ;;
+  
+    *)
+      # Two or more existing states for this volume (with $state_string_prefix)
+      # This implies more than one encryption passphrase for the volume, which is a conflict.
+      failure_message="Multiple states found for prefix: ${state_string_prefix_with_volume_name}${NEWLINE}"
+      failure_message+="Matching state strings:"
+      for matching_state_string in "${matching_state_strings[@]}"; do
+        failure_message+="${NEWLINE}  ${matching_state_string}"
+      done
+      report_fail "$failure_message"
+      exit 70
+      ;;
+  esac
+}
+
 function conditionally_mark_volume_as_pending_creation_DEPRECATED(){
   # Takes the volume and 1Password item key for a new user and, when appropriate, marks
   # in a system state that this volume needs to be created with the indicated passphrase.
