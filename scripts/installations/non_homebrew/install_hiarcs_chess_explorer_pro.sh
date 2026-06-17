a85bc5cb1f076a235a920d5624cc65c3a40cd928
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-06-17 10:25:47 -0700

    Create install_hiarcs_chess_explorer_pro.sh

---
+++ scripts/installations/non_homebrew/install_hiarcs_chess_explorer_pro.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+function conditionally_install_hiarcs_chess_explorer_pro() {
+  # Template for a Zsh function in Project GenoMac
+  report_start_phase_standard
+  report_end_phase_standard
+}
+
+function my_function() {
+  # Template for a Zsh function in Project GenoMac
+  report_start_phase_standard
+  report_end_phase_standard
+}
