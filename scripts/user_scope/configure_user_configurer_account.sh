0f8b32ee34765c93e37c4e49d7c4791807b4774d
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-06-21 21:59:38 -0700

    Update configure_user_configurer_account.sh

--- scripts/user_scope/configure_user_configurer_account.sh
+++ scripts/user_scope/configure_user_configurer_account.sh
@@ -1,5 +1,46 @@
 #!/usr/bin/env zsh
 
+function conditionally_mark_this_user_needs_initial_configuration() {
+  # Marks USER_CONFIGURER as needing initial configuration by GenoMac-user, if this hasn’t already been done.
+  # This is necessary as an explicit step because (a) Hypervisor-System automatically marks as needing initial
+  # configuration only new users that Hypervisor-System has created and (b) USER_CONFIGURER is not created by
+  # GenoMac-system but rather preexists in order to run Hypervisor-System.
+  
+  report_start_phase_standard
+  
+  run_if_system_has_not_done \
+    "$PERM_USER_CONFIGURER_HAS_BEEN_MARKED_AS_NEEDING_INITIAL_CONFIGURATION" \
+    mark_current_user_as_in_need_of_initial_config \
+    "Skipping marking USER_CONFIGURER as needing initial configuration, because this was marked in the past."
+    
+  report_end_phase_standard
+}
+
+function conditionally_instruct_user_configurer_to_configure_itself() {
+  # If USER_CONFIGURER hasn’t yet been configured using GenoMac-user, it must be done now.
+  report_start_phase_standard
+
+  local instruction_message="You must now use GenoMac-user to configure the current user account.${NEWLINE}I’ve opened a document explaining this process."
+
+  if ! test_whether_current_user_is_in_need_of_initial_config; then
+    report_to_log "USER_CONFIGURER isn’t in need of initial configuration. Moving on…"
+    report_end_phase_standard
+    return 0
+  fi
+
+  print_banner_text "HALT! Leave and come back"
+  
+  launch_app_and_prompt_user_to_act \
+    --show-doc "${GMS_DOCS_TO_DISPLAY}/USER_CONFIGURER_how_to_configure.md" \
+    --no-app \
+    "$instruction_message"
+
+  return 0
+  
+  
+  report_end_phase_standard
+}
+
 function conditionally_clone_genomac_user_using_HTTPS() {
   report_start_phase_standard
   
