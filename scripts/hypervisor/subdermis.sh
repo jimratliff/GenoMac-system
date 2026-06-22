f26e742fbe5b46c59f077255837fecaad8a7469a
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-06-22 10:30:36 -0700

    Update subdermis.sh

--- scripts/hypervisor/subdermis.sh
+++ scripts/hypervisor/subdermis.sh
@@ -16,7 +16,7 @@ function subdermis() {
   safe_source "${GMS_RESOURCE_INSTALL_SCRIPTS}/make_development_clones.sh"
   safe_source "${GMS_SETTINGS_SCRIPTS}/implement_systemwide_settings.sh"
   safe_source "${GMS_SETTINGS_SCRIPTS}/interactive_get_Mac_names_and_login_window_message.sh"
-  safe_source "${GMS_USER_SCOPE_SCRIPTS}/clone_genomac_user_repo.sh"
+  safe_source "${GMS_USER_SCOPE_SCRIPTS}/configure_user_configurer_account.sh"
   safe_source "${GMS_USER_SPAWNING_SCRIPTS}/spawn.sh"
 
   # TODO:
@@ -37,29 +37,39 @@ function subdermis() {
 
   # Mark the configuring user as a USER_CONFIGURER
   # Only USER_CONFIGURER runs GenoMac-system, therefore this user is USER_CONFIGURER
-  mark_current_user_as_user_configger                      # GenoMac-shared/scripts/helpers-state-xfer-btw-system-user.sh
+  mark_current_user_as_user_configger                        # GenoMac-shared/scripts/helpers-state-xfer-btw-system-user.sh
   
-  interactive_ensure_terminal_has_fda                      # GenoMac-shared/scripts/helpers-misc.sh
-  conditionally_adjust_path_for_homebrew                   # scripts/installations/homebrew/adjust_path_for_homebrew.sh
+  interactive_ensure_terminal_has_fda                        # GenoMac-shared/scripts/helpers-misc.sh
+  
+  conditionally_adjust_path_for_homebrew                     # scripts/installations/homebrew/adjust_path_for_homebrew.sh
+  
   conditionally_interactive_get_Mac_names_and_login_window_message # scripts/settings/interactive_get_Mac_names_and_login_window_message.sh
-  conditionally_interactive_sign_into_MAS                  # scripts/installations/homebrew/interactive_sign_into_MAS.sh
-  conditionally_install_rosetta                            # scripts/installations/install_rosetta.sh
-  conditionally_install_via_homebrew                       # scripts/installations/homebrew/install_via_homebrew.sh
-  conditionally_install_non_homebrew_apps                  # scripts/installations/non_homebrew/install_non_homebrew_apps.sh
-  conditionally_install_resources_systemwide               # scripts/installations/of_resources/install_resources.sh
-  conditionally_implement_systemwide_settings              # scripts/settings/implement_systemwide_settings.sh
+  
+  conditionally_interactive_sign_into_MAS                    # scripts/installations/homebrew/interactive_sign_into_MAS.sh
+  conditionally_install_rosetta                              # scripts/installations/install_rosetta.sh
+  conditionally_install_via_homebrew                         # scripts/installations/homebrew/install_via_homebrew.sh
+  conditionally_install_non_homebrew_apps                    # scripts/installations/non_homebrew/install_non_homebrew_apps.sh
+  conditionally_install_resources_systemwide                 # scripts/installations/of_resources/install_resources.sh
+  
+  conditionally_implement_systemwide_settings                # scripts/settings/implement_systemwide_settings.sh
+
+  ############### ↓↓↓ INITIALLY CONFIGURE THE USER_CONFIGURER ACCOUNT ↓↓↓ ###############
+  conditionally_mark_this_user_needs_initial_configuration   # scripts/user_scope/configure_user_configurer_account.sh
 
   # Default attributes for USER_CONFIGURER must be set before USER_CONFIGURER uses GenoMac-user to configure
   # USER_CONFIGURER’s user-scoped settings. Therefore we do it before GenoMac-user is even locally cloned
   # in order to enforce this condition.
-  conditionally_set_default_attributes_for_USER_CONFIGURER # scripts/spawn/default_attributes_for_user_configurer.sh
+  conditionally_set_default_attributes_for_USER_CONFIGURER   # scripts/spawn/default_attributes_for_user_configurer.sh
 
   # Clone GenoMac-user to ~/.genomac-user in preparation for USER_CONFIGURER to configure its own account
-  conditionally_clone_genomac_user_using_HTTPS             # scripts/user_scope/clone_genomac_user_repo.sh
-  
-  conditionally_create_user_accounts_for_this_Mac          # scripts/spawn/spawn.sh
+  conditionally_clone_genomac_user_using_HTTPS               # scripts/user_scope/configure_user_configurer_account.sh
+
+  conditionally_exit_for_user_configurer_to_configure_itself # scripts/user_scope/configure_user_configurer_account.sh
+
+  ############### ↓↓↓ SPAWNING NEW USERS ↓↓↓ ###############
+  conditionally_create_user_accounts_for_this_Mac            # scripts/spawn/spawn.sh
   conditionally_interactive_create_volumes_for_user_home_directories    # scripts/spawn/spawn-volume-creation.sh
-  display_users_to_be_initially_configured                 # GenoMac-shared/scripts/helpers-state-xfer-btw-system-user.sh
+  display_users_to_be_initially_configured                   # GenoMac-shared/scripts/helpers-state-xfer-btw-system-user.sh
   
   report_end_phase_standard
 }
