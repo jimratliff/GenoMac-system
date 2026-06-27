846fef84713ff33a3e27de0a069186316e19babc
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-06-26 20:23:16 -0700

    Update install_via_homebrew.sh

--- scripts/installations/homebrew/install_via_homebrew.sh
+++ scripts/installations/homebrew/install_via_homebrew.sh
@@ -12,7 +12,7 @@ function conditionally_install_via_homebrew() {
 }
 
 function install_via_homebrew() {
-  # Installs, upgrades, and (when necessary) removes no-longer-desired Homebrew packages.
+  # Installs, upgrades, and removes no-longer-desired Homebrew packages.
   #
   # For nonobvious reasons, brew install requires a password to install some casks
   # Preemptively calling `keep_sudo_alive` doesn’t prevent this
@@ -25,33 +25,34 @@ function install_via_homebrew() {
   report_action_taken "Suppressing quarantine flag on to-be-installed apps"
   export HOMEBREW_CASK_OPTS=--no-quarantine
 
+  local brewfile_path
   brewfile_path="${GMS_HOMEBREW}/Brewfile"
 
   # Updates Homebrew itself and its package definitions (formulae and casks) from the remote repository
-  report_action_taken "Updating Homebrew itself"
+  report_action_taken "Updating Homebrew itself and package definitions"
   brew update ; success_or_not
 
+  # Remove installed Homebrew items that are not declared in the aggregate Brewfile.
+  # This is the replacement for the old `brew bundle install --cleanup`.
+  report_action_taken "Removing Homebrew items not declared in Brewfile"
+  brew bundle cleanup --file="${brewfile_path}" --force ; success_or_not
+
   # Installs packages, etc. from Brewfile
-  # --cleanup is DEPRECATED with no replacement (at least as of 6/26/2026)
-  # --cleanup removes installed packages no longer called for by the Brewfile
-  # report_action_taken "Install new packages and remove no-longer-desired ones"
-  report_action_taken "Install new packages"
+  report_action_taken "Install and upgrading Homebrew items declared in Brewfile (and its children)"
   report_warning "Don’t walk away! You’ll be required to enter your administrator password for some apps."
-  # brew bundle install --cleanup --file="${brewfile_path}" ; success_or_not
   brew bundle install --file="${brewfile_path}" ; success_or_not
 
-  # Sets state to indicate that Homebrew has been used at least once to install apps
-  # This assumes basic existence of non-builtin apps on which other parts of GenoMac-system relies
-  set_genomac_system_state "$PERM_HOMEBREW_HAS_INSTALLED_APPS_AT_LEAST_ONCE"
+  # Reconcile again after install. This catches anything made removable by dependency changes.
+  report_action_taken "Removing Homebrew items not declared in Brewfile after install"
+  brew bundle cleanup --file="${brewfile_path}" --force ; success_or_not
 
-  # Upgrades, as necessary, installed packages
-  report_action_taken "Upgrades, as needed, installed packages"
-  brew upgrade ; success_or_not
-
-  # Removes stale lock files and outdated downloads for all formulae and casks, and removes old versions of installed formulae.
-  # Removes all downloads more than 120 days old. 
-  report_action_taken "Cleans up Homebrew installations"
+  # Removes stale lock files, outdated downloads, and old versions/caches
+  report_action_taken "Cleans up Homebrew caches and old versions"
   brew cleanup ; success_or_not
+
+  # Sets state to indicate that Homebrew has been used at least once to install apps
+  # This ensures basic existence of non-builtin apps on which other parts of GenoMac-system relies
+  set_genomac_system_state "$PERM_HOMEBREW_HAS_INSTALLED_APPS_AT_LEAST_ONCE"
 
   report_end_phase_standard
 
