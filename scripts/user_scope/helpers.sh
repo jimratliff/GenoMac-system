ec823619e840f832dd2c061ba377fd0a4f269991
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-07-10 21:01:09 -0700

    Update helpers.sh

--- scripts/user_scope/helpers.sh
+++ scripts/user_scope/helpers.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env zsh
 
-function list_File_Vault_enabled_users() {
-  # List File Vault–enabled users
+function list_FileVault_enabled_users() {
+  # Reports the short names of all users enabled to unlock FileVault.
+
   report_start_phase_standard
-  report "The following users are enabled to unlock File Vault on this startup volume:${NEWLINE}"
-  sudo fdesetup list | cut -d ',' -f 1
+
+  local filevault_enabled_users
+  filevault_enabled_users="$(sudo fdesetup list | cut -d ',' -f 1)"
+
+  _report \
+    --alert \
+    --message "The following users are enabled to unlock FileVault on this startup volume:${NEWLINE}${filevault_enabled_users}"
+
   report_end_phase_standard
 }
