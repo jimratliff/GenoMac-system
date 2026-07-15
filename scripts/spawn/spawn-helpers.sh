72e7ae59009d41944b22e59a27e16cb0b0c10a82
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-07-15 16:18:12 -0700

    Update spawn-helpers.sh

--- scripts/spawn/spawn-helpers.sh
+++ scripts/spawn/spawn-helpers.sh
@@ -102,6 +102,21 @@ function parent_of_users_home_directories() {
   print -r -- "$path_of_parent_of_home_directories"
 }
 
+function list_FileVault_enabled_users() {
+  # Reports the short names of all users enabled to unlock FileVault.
+
+  report_start_phase_standard
+
+  local filevault_enabled_users
+  filevault_enabled_users="$(sudo fdesetup list | cut -d ',' -f 1)"
+
+  _report \
+    --alert \
+    --message "The following users are enabled to unlock FileVault on this startup volume:${NEWLINE}${filevault_enabled_users}"
+
+  report_end_phase_standard
+}
+
 function get_short_name_from_user_spec_json() {
   local user_spec_json="$1"
   jq -r '.short_name' <<<"$user_spec_json"
@@ -271,6 +286,43 @@ function get_GitHub_PAT_for_GenoMac_private_from_1Password_vault() {
   fi
 
   print -r -- "$github_pat"
+  report_end_phase_standard
+}
+
+function set_user_picture() {
+  # Sets the account picture for an existing local user.
+  #
+  # Usage:
+  #   set_user_picture short_name /absolute/path/to/image
+
+  report_start_phase_standard
+
+  local short_name="${1:?MISSING short_name}"
+  local picture_path="${2:?MISSING picture_path}"
+  local user_record="/Users/${short_name}"
+
+  if [[ "${picture_path}" != /* ]]; then
+    report_fail "ERROR: User-picture path must be absolute: ${picture_path}"
+    return 1
+  fi
+
+  if [[ ! -f "${picture_path}" ]]; then
+    report_fail "ERROR: User-picture file does not exist: ${picture_path}"
+    return 1
+  fi
+
+  if ! dscl . -read "${user_record}" RecordName &>/dev/null; then
+    report_fail "ERROR: No local user exists with short name: ${short_name}"
+    return 1
+  fi
+
+  # An embedded JPEGPhoto ordinarily overrides the path stored in Picture.
+  if sudo dscl . -read "${user_record}" JPEGPhoto &>/dev/null; then
+    sudo dscl . -delete "${user_record}" JPEGPhoto
+  fi
+
+  sudo dscl . -create "${user_record}" Picture "${picture_path}"
+
   report_end_phase_standard
 }
 
