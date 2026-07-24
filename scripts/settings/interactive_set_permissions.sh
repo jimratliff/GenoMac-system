1f21058aaaee6da6939a9ec4e74e83220bb3eaf2
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-07-23 21:50:37 -0700

    Update interactive_set_permissions.sh

--- scripts/settings/interactive_set_permissions.sh
+++ scripts/settings/interactive_set_permissions.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env zsh
 
 function conditional_interactive_ensure_terminal_has_fda() {
-  # Template for a Zsh function in Project GenoMac
+  # Ensures that current terminal app has Full Disk Access, unless this has already been done earlier this session.
   report_start_phase_standard
   
   run_if_system_has_not_done \
     "$SESH_TERMINAL_FULL_DISK_ACCESS_HAS_BEEN_ASSURED" \
 	  interactive_ensure_terminal_has_fda \
-    "Skipping setting Full Disk Access for current terminal, because this has already been assured earlier this session."
+    "Skipping ensuring Full Disk Access for current terminal, because this has already been assured earlier this session."
     
   report_end_phase_standard
 }
