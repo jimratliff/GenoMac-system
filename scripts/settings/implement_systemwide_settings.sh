54afc4c29f8738b5e0152ef9907c743c93407e4f
Jim Ratliff <8410716+jimratliff@users.noreply.github.com>
2026-08-19 07:02:13 -0700

    Update implement_systemwide_settings.sh

--- scripts/settings/implement_systemwide_settings.sh
+++ scripts/settings/implement_systemwide_settings.sh
@@ -55,11 +55,7 @@ function implement_systemwide_settings() {
   sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName ; success_or_not
 
   # Enable Touch ID authentication for sudo
-  # See https://dev.to/siddhantkcode/enable-touch-id-authentication-for-sudo-on-macos-sonoma-14x-4d28
-  # As of macOS Sonoma, the settings can be added to a separate file /etc/pam.d/sudo_local, which isn’t
-  # overwritten during updates, allowing Touch ID to remain enabled for sudo commands consistently.
-  report_action_taken "Enable Touch ID authentication for sudo"
-  sed -e 's/^#auth/auth/' /etc/pam.d/sudo_local.template | sudo tee /etc/pam.d/sudo_local ; success_or_not
+  enable_Touch_ID_authentication_for_sudo
   
   report_end_phase_standard
 
